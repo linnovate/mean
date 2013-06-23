ddd0b3b9a26a7945f53a0285d81ea34197b3daf2
Amos Haviv <mail@amoshaviv.com>
2013-06-23 20:49:51 +0300

    removing the logger module

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,12 +5,9 @@
 var mongoose = require('mongoose')
   , async = require('async')
   , _ = require('underscore')
-  , logger = require('mean-logger')
 
 
 exports.render = function(req, res){
-  logger.log("test");
-
   res.render('index', {
     user: req.user ? JSON.stringify(req.user) : "null"
   })
