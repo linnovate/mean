3688970aa9a328e13d8a47f16770542967aca3f9
Amos Haviv <mail@amoshaviv.com>
2013-07-09 20:36:41 +0300

    Adding Bower Support

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,10 @@
 //Setting up route
 window.app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
+	when('/articles', { templateUrl: 'views/articles/list.html' }).
+	when('/articles/create', { templateUrl: 'views/articles/create.html' }).
+	when('/articles/:articleId/edit', { templateUrl: 'views/articles/edit.html' }).
+	when('/articles/:articleId', { templateUrl: 'views/articles/view.html' }).
 	when('/', { templateUrl: 'views/index.html' }).
 	otherwise({redirectTo: '/'});
 }]);
