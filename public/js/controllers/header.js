6b0109e5562f2ed428702a9da0e085eabbda072c
Amos Haviv <mail@amoshaviv.com>
2013-07-25 16:17:09 +0300

    Articles Example Fix

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -1,5 +1,15 @@
 function HeaderController($scope, $location, Global) {
 	$scope.global = Global;
+	$scope.menu = [
+		{
+			"title": "Articles",
+			"link": "articles"
+		},
+		{
+			"title": "Create New Article",
+			"link": "articles/create"
+		}
+	];
 
 	$scope.init = function() {
 
