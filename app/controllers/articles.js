5610d78c70acfe884c960b2058403dd2fb3e9c14
RafaDev7 <rafadev7@gmail.com>
2013-09-08 06:28:32 -0300

    Removing an unecessary code line

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -11,8 +11,6 @@ var mongoose = require('mongoose'),
  * Find article by id
  */
 exports.article = function(req, res, next, id) {
-    var User = mongoose.model('User');
-
     Article.load(id, function(err, article) {
         if (err) return next(err);
         if (!article) return next(new Error('Failed to load article ' + id));
