8c44e606e0c74c95caba222733ef3b259de4d483
davegonzalez <gonzalez.dalex@gmail.com>
2013-10-03 20:55:47 -0700

    Moved config.js file into app.js to move items out of window scope

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,27 @@
-window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+        when('/articles', {
+            templateUrl: 'views/articles/list.html'
+        }).
+        when('/articles/create', {
+            templateUrl: 'views/articles/create.html'
+        }).
+        when('/articles/:articleId/edit', {
+            templateUrl: 'views/articles/edit.html'
+        }).
+        when('/articles/:articleId', {
+            templateUrl: 'views/articles/view.html'
+        }).
+        when('/', {
+            templateUrl: 'views/index.html'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+}]).config(['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('!');
+}]);
 
 angular.module('mean.system', []);
 angular.module('mean.articles', []);
