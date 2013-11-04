673ac8170421f85331f577fa28912cddb2090336
Rudi Starcevic <ooly.me@gmail.com>
2013-11-04 11:26:49 +0800

    Add Karma unit tests

---
+++ test/karma/unit/controllers/index.spec.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+    describe('MEAN controllers', function() {
+
+        describe('IndexController', function() {
+
+            // Load the controllers module
+            beforeEach(module('mean'));
+
+            var scope,
+                IndexController;
+
+            beforeEach(inject(function($controller, $rootScope) {
+                scope = $rootScope.$new();
+
+                IndexController = $controller('IndexController', {
+                    $scope: scope
+                });
+            }));
+
+            it('should expose some global scope', function() {
+
+                expect(scope.global).toBeTruthy();
+
+            });
+
+        });
+
+    });
+
+})();
