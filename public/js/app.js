f2a85f8dcd2ba5892611d2fb2e7d1f6e9c21dcc5
Jonathan Dizdarevic <dizzda@gmail.com>
2013-12-07 19:30:41 +0100

    Update to AngularJS 1.2.4

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles']);
 
 angular.module('mean.system', []);
 angular.module('mean.articles', []);
