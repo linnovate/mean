ef50e5da0da9061935c9ef60b0ea1b9bf46eaf91
Amos Haviv <mail@amoshaviv.com>
2013-12-23 19:36:55 +0200

    Fixing Facebook Redirect

--- config/env/development.js
+++ config/env/development.js
@@ -4,8 +4,8 @@ module.exports = {
         name: "MEAN - A Modern Stack - Development"
     },
     facebook: {
-        clientID: "APP_ID",
-        clientSecret: "APP_SECRET",
+        clientID: "455818271176818",
+        clientSecret: "db6c579d1fcdc9a3980bc4aade0e3aaf",
         callbackURL: "http://localhost:3000/auth/facebook/callback"
     },
     twitter: {
