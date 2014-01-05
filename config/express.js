add000d3a19ff5d157e32ecb446aaf3e761e83b6
Liran Tal <liran.tal@gmail.com>
2014-01-05 23:14:11 +0200

    updating documentation

--- config/express.js
+++ config/express.js
@@ -21,6 +21,8 @@ module.exports = function(app, passport, db) {
         filter: function(req, res) {
             return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
         },
+        // Levels are specified in a range of 0 to 9, where-as 0 is
+        // no compression and 9 is best compression, but slowest
         level: 9
     }));
 
