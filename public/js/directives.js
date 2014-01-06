bed3fb632680eb59e4ecd7b63e5ff09a66c6ab2c
Aleks Rozman <aleks.rozman@gmail.com>
2014-01-05 21:37:12 -0600

    Add sample directive
    Use <mean-label>[Text]</mean-label>

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,1 +1,10 @@
 'use strict';
+
+angular.module('mean').directive('meanLabel', function() {
+    return {
+        restrict : 'EA',
+        replace : true,
+        transclude : true,
+        template : '<span class=\'label label-default\' ng-transclude></span>'
+    };
+});
