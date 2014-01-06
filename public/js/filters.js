8b49ff810689cc0f07bd6150ab51098023864c38
Aleks Rozman <aleks.rozman@gmail.com>
2014-01-05 21:39:08 -0600

    Add sample filter
    Identify if passed in date is less than a day old (or passed in value)

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,1 +1,17 @@
 'use strict';
+
+angular.module('mean').filter('isnew', function() {
+    return function(input, days) {
+        var timestamp = Date.parse(input);
+        if (isNaN(timestamp)) {
+            return false;
+        }
+        else {
+            var diffDays = Math.round(Math.abs(((new Date(timestamp)).getTime() -
+                                                (new Date()).getTime())/(24*60*60*1000)));
+            var outdated = 1;
+            if(days) outdated = days;
+            return (diffDays < outdated);
+        }
+    };
+});
