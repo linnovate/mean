d563475c6b2497250d908d5dc90063d3aaa62d30
yonatan <yonatan@yonatan-hp.(none)>
2014-01-19 20:40:00 +0200

    cleanup

--- modules/404/app.js
+++ modules/404/app.js
@@ -1,11 +1,12 @@
 
+
 module.exports = function() {
 	mean.register('404', function (app, middleware) {	  	 	 
-		//very heavy weight to ensure it is last
+		//very heavy weight to ensure it is last		    
 		middleware.add('after',999, function(req,res,next) {
       res.status(404).render('404', {
         url: req.originalUrl,
-        error: 'Not found!!!!!!!'
+        error: 'Not found!'
       });
    	})	  
 	});
