1623376b83eb3dac2653d5c34f6981ce76ada30d
Yonatan Ellman <yonatan@nodeside.com>
2014-01-27 16:28:39 +0200

    cleanup of the dynamically built js

--- modules/public/js/controllers.js
+++ modules/public/js/controllers.js
@@ -1,6 +1,1 @@
-//Mon Jan 27 2014 16:13:55 GMT+0200 (IST)
-'use strict';
 
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
-    $scope.global = Global;
-}]);
