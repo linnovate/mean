3e5c443245c52faefdc74b03912cc6923e94d07b
Yonatan Ellman <yonatan@nodeside.com>
2014-01-28 11:46:22 +0200

    deleting extra wasteful code

--- modules/public/js/sys/controllers.js
+++
@@ -1,7 +0,0 @@
-//Tue Jan 28 2014 11:35:31 GMT+0200 (IST)
-'use strict';
-
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global','$route', function ($scope, Global, $route) {
-    console.log($route);
-    $scope.global = Global;
-}]);
