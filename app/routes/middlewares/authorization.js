ad9124bd0a2f3a8eafca830875c226adbc6170cb
Liran Tal <liran.tal@gmail.com>
2014-02-01 11:37:11 +0200

    do not allow the logged-in users to re-signin, to sign-up or create users while being logged in

--- app/routes/middlewares/authorization.js
+++ app/routes/middlewares/authorization.js
@@ -7,5 +7,26 @@ exports.requiresLogin = function(req, res, next) {
     if (!req.isAuthenticated()) {
         return res.send(401, 'User is not authorized');
     }
+    
     next();
 };
+
+/**
+ * [requiresAnonymous description]
+ * @param  {object} req Request object
+ * @param  {object} res Response object
+ * @return {[type]}     Redirect a user to back referred page or call the next
+ *                      middleware
+ */
+exports.requiresAnonymous = function(req, res, next) {
+    // If user is authenticated, redirect back to referering URL
+    if (req.isAuthenticated()) {
+        var httpReferrer = req.get('Referrer');
+        if (httpReferrer && httpReferrer.length > 0)
+            res.redirect(httpReferrer);
+        else
+            res.redirect('/');
+    }
+
+    next();
+}
