40a21b4e5a86696b718fa81ddb430be2d9fefa4a
elmalto <malte.buecken@gmail.com>
2014-02-06 11:46:28 +0100

    added grunt SCSS support, switched from ngRoute to UI-Router for nested views

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'mean.system', 'mean.articles']);
 
 angular.module('mean.system', []);
 angular.module('mean.articles', []);
