0b9b70da139bdfe0d99d93cae97575b982f4d063
ehud shahak <udisun@gmail.com>
2014-02-19 21:24:38 +0200

    fixed jshints indentations warnings

--- public/js/config.js
+++ public/js/config.js
@@ -11,29 +11,29 @@ angular.module('mean').config(['$stateProvider', '$urlRouterProvider',
       .state('all articles', {
         url: '/articles',
         templateUrl: 'views/articles/list.html'
-      })
+    })
       .state('create article', {
         url: '/articles/create',
         templateUrl: 'views/articles/create.html'
-      })
+    })
       .state('edit article', {
         url: '/articles/:articleId/edit',
         templateUrl: 'views/articles/edit.html'
-      })
+    })
       .state('article by id', {
         url: '/articles/:articleId',
         templateUrl: 'views/articles/view.html'
-      })
+    })
       .state('home', {
         url: '/',
         templateUrl: 'views/index.html'
-      });
-  }
+    });
+}
 ]);
 
 //Setting HTML5 Location Mode
 angular.module('mean').config(['$locationProvider',
   function($locationProvider) {
     $locationProvider.hashPrefix('!');
-  }
+}
 ]);
