5df3a054d7bff4c6d248a9be56d639545b28cce4
Drew Fyock <fyockm@gmail.com>
2014-03-23 14:57:52 -0400

    #332 fix travis

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ var _ = require('lodash'),
 
 // Load configurations
 // Set the node environment variable if not set before
-process.env.NODE_ENV = ~fs.readdirSync('./env').map(function(file) {
+process.env.NODE_ENV = ~fs.readdirSync('./server/config/env').map(function(file) {
 	return file.slice(0, -3);
 }).indexOf(process.env.NODE_ENV) ? process.env.NODE_ENV : 'development';
 
