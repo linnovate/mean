99d8776c57dc146f45decae138b7189489e85eb7
Yonatan Ellman <yonatan@nodeside.com>
2014-03-26 15:34:14 +0200

    asset refactor part 1

--- public/system/system.js
+++ public/system/system.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'mean.system', 'mean.articles']);
-
 angular.module('mean.system', ['mean.controllers.login','mean-factory-interceptor']);
+
 angular.module('mean.articles', []);
