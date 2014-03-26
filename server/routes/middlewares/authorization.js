e87663e39f576fc921201b9212780844d3aee9f9
Yonatan Ellman <yonatan@nodeside.com>
2014-03-27 01:41:36 +0200

    very basic roles access with hasRole and auth.isAdmin function

--- server/routes/middlewares/authorization.js
+++ server/routes/middlewares/authorization.js
@@ -9,3 +9,14 @@ exports.requiresLogin = function(req, res, next) {
     }
     next();
 };
+
+/**
+ * Generic require Admin routing middleware
+ * Basic Role checking - future release with full permission system
+ */
+exports.requiresAdmin = function(req, res, next) {
+    if (!req.isAuthenticated() || !req.user.hasRole('admin')) {
+        return res.send(401, 'User is not authorized');
+    }
+    next();
+};
