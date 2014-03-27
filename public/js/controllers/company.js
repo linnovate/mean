3c308ae77c709e39ef963cca7430f157f17e16b9
cahavar <cahavar@55yali.com>
2014-03-27 17:54:02 +0800

    完成注册页面基本样式

---
+++ public/js/controllers/company.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('mean.company').controller('CompanyController', ['$scope', 'Global', function ($scope, Global) {
+    $scope.global = Global;
+
+    $scope.province = ['广东'];
+
+}]);
