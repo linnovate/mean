ade64861fa4e755973d0fbaa5867cbdafdade224
cahavar <cahavar@55yali.com>
2014-04-03 17:37:21 +0800

    移除自定义的angularjs模块在mean应用中的初始化，改为按需要初始化，避免加载不需要的模块。添加company布局。

--- public/js/controllers/company.js
+++ public/js/controllers/company.js
@@ -1,42 +1,44 @@
 'use strict';
 
-angular.module('company')
-  .controller('RegisterController', ['$scope', 'PCSelector', function($scope, PCSelector) {
-    $scope.pcSelector = new PCSelector.PCSelector();
-  }]);
-angular.module('company')
-    .controller('groupsController',['$scope','$http', function($scope,$http) {
-        $http.get('/group/getgroups').success(function(data,status){
-            $scope.groups =data;
-        }).error(function(data,status){
-            alert('组件获取失败！');
+var companyApp = angular.module('company', ['pcselector']);
+
+companyApp.controller('RegisterController', ['$scope', 'PCSelector', 
+    function($scope, PCSelector) {
+        $scope.pcSelector = new PCSelector.PCSelector();
+    }
+]);
+companyApp.controller('groupsController',['$scope','$http', function($scope,$http) {
+    $http.get('/group/getgroups').success(function(data,status){
+        $scope.groups =data;
+    }).error(function(data,status){
+        alert('组件获取失败！');
+    });
+    $scope.selected =[];
+    $scope.group_next = function(){
+        $scope.selected.length = 0;
+        angular.forEach($scope.groups, function(value, key){
+            if(value.select==='1'){
+                $scope.selected.push(value.type);
+            }
         });
-        $scope.selected =[];
-        $scope.group_next = function(){
-            $scope.selected.length = 0;
-            angular.forEach($scope.groups, function(value, key){
-                if(value.select==='1'){
-                    $scope.selected.push(value.type);
+        try{
+            $http({
+                method: 'post',
+                url: '/company/groupSelect',
+                data:{
+                    selected : $scope.selected
                 }
+            }).success(function(data, status) {
+                //alert('success');
+                // console.log(data);
+                alert("选择组件成功！");
+                window.location.href="/company/invite";
+            }).error(function(data, status) {
+                alert("数据发生错误！");
             });
-            try{
-                $http({
-                    method: 'post',
-                    url: '/company/groupSelect',
-                    data:{
-                        selected : $scope.selected
-                    }
-                }).success(function(data, status) {
-                    //alert('success');
-                    // console.log(data);
-                    alert("选择组件成功！");
-                    window.location.href="/company/invite";
-                }).error(function(data, status) {
-                    alert("数据发生错误！");
-                });
-            }
-            catch(e){
-                console.log(e);
-            }
-        };
-    }]);
+        }
+        catch(e){
+            console.log(e);
+        }
+    };
+}]);
