51abac8d28fea589f62f05c0c781e6d625cabd2d
Drew Fyock <fyockm@gmail.com>
2014-04-04 13:17:05 -0400

    Merge branch 'grunt' of https://github.com/fyockm/mean-cli.git into master

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    mochaTest: {
+      options: {
+        require: ['should'],
+        timeout: 3000,
+        ignoreLeaks: false,
+        reporter: 'spec'
+      },
+      src: ['test/*/*.js']
+    },
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      src: ['index.js', 'lib/*.js', 'bin/*', '!bin/logo.txt']
+    }
+  });
+
+  // Load grunt plugins for modules
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Register tasks
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
+};
