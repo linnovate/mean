0a9ad5c7fe369162f40b53dea5737049d45e07eb
cahavar <cahavar@55yali.com>
2014-04-08 16:29:35 +0800

    解决了因改动数据结构而造成passport配置错误的问题

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -99,7 +99,7 @@ exports.signout = function(req, res) {
  * Session
  */
 exports.loginSuccess = function(req, res) {
-    req.session.username = req.body.email;
+    req.session.username = req.body.username;
     res.redirect('/users/edit/info');
 };
 
