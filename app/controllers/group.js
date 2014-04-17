e3112796f493f93ab761bea27c22ef3b2336570b
afei <afei@55yali.com>
2014-04-17 15:42:30 +0800

    修改组件选择页面的布局，不显示虚拟组

--- app/controllers/group.js
+++ app/controllers/group.js
@@ -15,54 +15,11 @@ var mongoose = require('mongoose'),
 
 exports.saveGroups = function(req,res) {
     res.send('save');
-   /* var _length =req.body.group.length();
-    for(var _i=0;_i<_length;_i++){
-
-    }
-    var group = new Group();
-    group.name = req.body.name;
-    company.info.city.province = req.body.province;
-    company.info.city.city = req.body.city;
-    company.info.address = req.body.address;
-    company.info.linkman = req.body.linkman;
-    company.info.lindline.areacode = req.body.areacode;
-    company.info.lindline.number = req.body.number;
-    company.info.lindline.extension = req.body.extension;
-    company.info.phone = req.body.phone;
-    company.email.host = req.body.host;
-    company.email.domain[0] = req.body.domain;
-
-    company.provider = 'company';
-    company.save(function(err) {
-        if (err) {
-            console.log(err);
-            //检查信息是否重复
-            switch (err.code) {
-                case 11000:
-                    break;
-                case 11001:
-                    res.status(400).send('该公司已经存在!');
-                    break;
-                default:
-                    break;
-            }
-            return res.render('company/company_signup', {
-                company: company
-            });
-        }
-        //发送邮件
-        mail.sendCompanyActiveMail(company.email.host+'@'+company.email.domain[0],company.info.name);
-        res.render('company/company_wait', {
-            title: '等待验证',
-            message: '您的申请信息已经提交,等验证通过后我们会向您发送一封激活邮件,请注意查收!'
-        });
-    });*/
 };
 
 
 //返回组件模型里的所有组件,待HR选择
 exports.getGroups = function(req,res) {
-  console.log('ok');
   Group.find(null,function(err,group){
       if (err) {
           console.log(err);
@@ -73,9 +30,10 @@ exports.getGroups = function(req,res) {
       var groups = [];
 
       for(var i = 0; i < _length; i++ ){
-        groups.push({'id':group[i].gid,'type':group[i].group_type,'select':'0'});
+        if(group[i].gid!=0){
+          groups.push({'id':group[i].gid,'type':group[i].group_type,'select':'0'});
+        }
       }
-      console.log(groups);
       res.send(groups);
   });
 };
