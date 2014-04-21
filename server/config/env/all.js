bbf267fc3fa77fd08eeab2692ac9a61bbe577970
Adrian Edworthy <git@adrianedworthy.com>
2014-04-21 23:18:56 +0100

    Convert tabs to spaces.

--- server/config/env/all.js
+++ server/config/env/all.js
@@ -4,14 +4,14 @@ var path = require('path');
 var rootPath = path.normalize(__dirname + '/../../..');
 
 module.exports = {
-	root: rootPath,
-	port: process.env.PORT || 3000,
-	db: process.env.MONGOHQ_URL,
-	templateEngine: 'swig',
+    root: rootPath,
+    port: process.env.PORT || 3000,
+    db: process.env.MONGOHQ_URL,
+    templateEngine: 'swig',
 
-	// The secret should be set to a non-guessable string that
-	// is used to compute a session hash
-	sessionSecret: 'MEAN',
-	// The name of the MongoDB collection to store sessions in
-	sessionCollection: 'sessions'
+    // The secret should be set to a non-guessable string that
+    // is used to compute a session hash
+    sessionSecret: 'MEAN',
+    // The name of the MongoDB collection to store sessions in
+    sessionCollection: 'sessions'
 };
