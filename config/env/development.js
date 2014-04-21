b26ac740c3be5d9237bae1300c8ee82660839540
nicoJiang <nicoJiang@55yali.com>
2014-04-21 10:56:08 +0800

    修改数据库地址

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://127.0.0.1/mean-dev',
+    db: 'mongodb://192.168.2.109/mean-dev',
     app: {
         name: 'MEAN - A Modern Stack - Development'
     }
