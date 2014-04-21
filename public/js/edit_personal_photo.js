bfd39d0fc43b45fb3e7e52d4669fc2a9c35f9c51
cahavar <cahavar@55yali.com>
2014-04-21 20:12:02 +0800

    增加上传前的裁剪效果

--- public/js/edit_personal_photo.js
+++ public/js/edit_personal_photo.js
@@ -7,6 +7,7 @@
       var options = {
         success: function(data, status) {
           $('#edit_img').attr('src', '/img/user/photo/temp/' + data.img);
+          $('#preview').attr('src', '/img/user/photo/temp/' + data.img);
         }
       };
 
@@ -18,6 +19,25 @@
     temp_photo.change(function() {
       temp_photo_form.submit();
     });
+
+    function showPreview(coords)
+    {
+      var rx = 100 / coords.w;
+      var ry = 100 / coords.h;
+
+      $('#preview').css({
+        width: Math.round(rx * 500) + 'px',
+        height: Math.round(ry * 370) + 'px',
+        marginLeft: '-' + Math.round(rx * coords.x) + 'px',
+        marginTop: '-' + Math.round(ry * coords.y) + 'px'
+      });
+    }
+
+    $('#edit_img').Jcrop({
+      onChange: showPreview,
+      aspectRatio: 1
+    });
+
   });
 
 }(jQuery));
