8d2094a3c1583a823edcc07d50e4a1c03c88cdb1
William Golden <egdelwonk@gmail.com>
2014-04-24 10:29:31 -0500

    Removed grunt force option.
    Since we are no longer linting in production by default, this will not be necessary.

--- Gruntfile.js
+++ Gruntfile.js
@@ -105,9 +105,6 @@ module.exports = function(grunt) {
     //Load NPM tasks
     require('load-grunt-tasks')(grunt);
 
-    //Making grunt default to force in order not to break the project.
-    grunt.option('force', true);
-
     //Default task(s).
     if (process.env.NODE_ENV === 'production') {
         grunt.registerTask('default', ['cssmin', 'uglify', 'concurrent']);
