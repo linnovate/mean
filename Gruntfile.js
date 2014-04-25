23642d0f39ca86f900d5ed8beaa0ca00c06b487c
Drew Fyock <fyockm@gmail.com>
2014-04-25 12:02:55 -0400

    update grunt paths, exlude node_modules from nodemon

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var paths = {
+    js: ['*.js', 'server/**/*.js', 'public/**/*.js', 'test/**/*.js', '!test/coverage/**', '!public/system/lib/**'],
+    html: ['public/**/views/**', 'server/views/**'],
+    css: ['public/**/css/**', '!public/system/lib/**']
+};
+
 module.exports = function(grunt) {
 
     if (process.env.NODE_ENV !== 'production') {
@@ -12,20 +18,20 @@ module.exports = function(grunt) {
         assets: grunt.file.readJSON('server/config/assets.json'),
         watch: {
             js: {
-                files: ['*.js', 'server/**/*.js', 'public/**/*.js', 'test/**/*.js'],
+                files: paths.js,
                 tasks: ['jshint'],
                 options: {
                     livereload: true
                 }
             },
             html: {
-                files: ['public/**/views/**', 'server/views/**'],
+                files: paths.html,
                 options: {
                     livereload: true
                 }
             },
             css: {
-                files: ['public/**/css/**'],
+                files: paths.css,
                 tasks: ['csslint'],
                 options: {
                     livereload: true
@@ -34,7 +40,7 @@ module.exports = function(grunt) {
         },
         jshint: {
             all: {
-                src: ['*.js', 'server/**/*.js', 'public/**/*.js', 'test/**/*.js', '!test/coverage/**/*.js', '!public/system/lib/**'],
+                src: paths.js,
                 options: {
                     jshintrc: true
                 }
@@ -53,7 +59,7 @@ module.exports = function(grunt) {
                 csslintrc: '.csslintrc'
             },
             all: {
-                src: ['public/**/css/**/*', '!public/system/lib/**']
+                src: paths.css
             }
         },
         cssmin: {
@@ -66,7 +72,7 @@ module.exports = function(grunt) {
                 script: 'server.js',
                 options: {
                     args: [],
-                    ignore: ['public/**'],
+                    ignore: ['public/**', 'node_modules/**'],
                     ext: 'js,html',
                     nodeArgs: ['--debug'],
                     delayTime: 1,
@@ -117,9 +123,8 @@ module.exports = function(grunt) {
 
     //Test task.
     grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
-    
+
     // For Heroku users only.
     // Docs: https://github.com/linnovate/mean/wiki/Deploying-on-Heroku
     grunt.registerTask('heroku:production', ['jshint', 'csslint', 'cssmin', 'uglify']);
-    
 };
