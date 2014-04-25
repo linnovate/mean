eec05866ece2e662dc57bfd1696c8f3ebdb14271
Ryan Latham <ryalatham@gmail.com>
2014-04-25 15:07:47 -0400

    Converted tabs to spaces

--- server/config/env/all.js
+++ server/config/env/all.js
@@ -4,15 +4,15 @@ var path = require('path');
 var rootPath = path.normalize(__dirname + '/../../..');
 
 module.exports = {
-	root: rootPath,
-	port: process.env.PORT || 3000,
-	db: process.env.MONGOHQ_URL,
+    root: rootPath,
+    port: process.env.PORT || 3000,
+    db: process.env.MONGOHQ_URL,
     gruntTasks: ['jshint', 'csslint', 'concurrent'],
-	templateEngine: 'swig',
+    templateEngine: 'swig',
 
-	// The secret should be set to a non-guessable string that
-	// is used to compute a session hash
-	sessionSecret: 'MEAN',
-	// The name of the MongoDB collection to store sessions in
-	sessionCollection: 'sessions'
+    // The secret should be set to a non-guessable string that
+    // is used to compute a session hash
+    sessionSecret: 'MEAN',
+    // The name of the MongoDB collection to store sessions in
+    sessionCollection: 'sessions'
 };
