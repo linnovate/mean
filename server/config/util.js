6462f1585ba30461603aec369bf3ee53c63ade51
Drew Fyock <fyockm@gmail.com>
2014-04-26 21:31:03 -0400

    rewrite exports.walk

--- server/config/util.js
+++ server/config/util.js
@@ -4,14 +4,15 @@ var fs = require('fs'),
     path = require('path');
 
 // recursively walk modules path and callback for each file
-exports.walk = function(modulesPath, excludeDir, callback) {
+var walk = function(modulesPath, excludeDir, callback) {
     fs.readdirSync(modulesPath).forEach(function(file) {
         var newPath = path.join(modulesPath, file);
         var stat = fs.statSync(newPath);
         if (stat.isFile() && /(.*)\.(js|coffee)$/.test(file)) {
             callback(newPath);
         } else if (stat.isDirectory() && file !== excludeDir) {
-            exports.walk(newPath, excludeDir, callback);
+	    walk(newPath, excludeDir, callback);
         }
     });
 };
+exports.walk = walk;
