707fac3e7b3bff16566999f89f5d64a2bf28c280
afei <afei@55yali.com>
2014-05-06 16:44:33 +0800

    修改样式

--- public/js/controllers/tabviewGroup.js
+++ public/js/controllers/tabviewGroup.js
@@ -375,7 +375,6 @@ tabViewGroup.controller('infoController', ['$http', '$scope',function($http, $sc
                     //TODO:更改对话框
                     if(data.result === 1) {
                         alert('信息修改成功！');
-                        window.location.reload();
                     }
                     else
                         alert(data.msg);
