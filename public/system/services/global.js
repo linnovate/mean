30ae8c3fc70740f375c6e83fb3401f6f00bc933d
Yonatan Ellman <yonatan@nodeside.com>
2014-05-07 17:23:33 +0300

    authenticated false when no roles

--- public/system/services/global.js
+++ public/system/services/global.js
@@ -1,13 +1,12 @@
 'use strict';
-
 //Global service for global variables
 angular.module('mean.system').factory('Global', [
+
     function() {
         var _this = this;
-
         _this._data = {
             user: window.user,
-            authenticated: window.user.roles
+            authenticated: (window.user.roles ? window.user.roles : false)
         };
         return _this._data;
     }
