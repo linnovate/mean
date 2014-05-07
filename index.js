3a9da793509ed31e2205bf0c20b9f2fad6773cd9
Drew Fyock <fyockm@gmail.com>
2014-05-07 11:16:17 -0400

    rename mean.js for easier merge going forward

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 var meanVersion = require(process.cwd() + '/package.json').version;
 
-if (meanVersion > '0.3.0') {
-	switch (meanVersion) {
-		case '0.3.1':
-			module.exports = require('./lib/mean0.3.1');
-			break;
-		default:
-			module.exports = require('./lib/mean0.3.2');
-			break;
-	}
+if (meanVersion < '0.3.2') {
+	if (meanVersion === '0.3.1')
+		module.exports = require('./lib/mean0.3.1');
+	else
+		module.exports = require('./lib/mean0.3.0');
 } else {
-	//all older versions of mean
+	// current version
 	module.exports = require('./lib/mean');
 }
