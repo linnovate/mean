793b9e0b5d61e81df674c9177856bb258c28cf76
Keith <keith@keithfimreite.com>
2014-05-09 18:07:54 -0700

    Add global.IsAdmin
    Quickly check to see logged in user is an admin.

--- public/system/services/global.js
+++ public/system/services/global.js
@@ -7,7 +7,8 @@ angular.module('mean.system').factory('Global', [
         var _this = this;
         _this._data = {
             user: window.user,
-            authenticated: window.user && window.user.roles
+            authenticated: window.user && window.user.roles,
+            isAdmin: (window.user && window.user.roles) && window.user.roles.indexOf('admin') > -1
         };
         return _this._data;
     }
