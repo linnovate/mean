ba736da315ce2d2fc10f7c6ea7e1f5ea3f77d575
Serkan Yerşen <serkanyersen@gmail.com>
2014-05-11 12:23:06 -0700

    When logged in with github, user.roles is empty
    `ng-show` and `ng-hide` evaluates `[]` as `false` which is not the desired outcome. forcing user.authenticated to be `boolean` resolves the issue.

--- public/system/services/global.js
+++ public/system/services/global.js
@@ -7,7 +7,7 @@ angular.module('mean.system').factory('Global', [
         var _this = this;
         _this._data = {
             user: window.user,
-            authenticated: window.user && window.user.roles,
+            authenticated: Boolean(window.user && window.user.roles),
             isAdmin: (window.user && window.user.roles) && window.user.roles.indexOf('admin') > -1
         };
         return _this._data;
