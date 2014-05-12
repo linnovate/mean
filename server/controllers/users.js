6861372bbb2493e8f0de93e618ba3406b25a168d
Keith <keith@keithfimreite.com>
2014-05-12 01:08:19 -0700

    Update users.js

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -80,6 +80,46 @@ exports.register = function(req, res, next) {
         res.status(200);
     });
 };
+
+/**
+ * Create user
+ */
+exports.create = function(req, res, next) {
+    var user = new User(req.body);
+
+    user.provider = 'local';
+
+    // because we set our user.provider to local our models/user.js validation will always be true
+    req.assert('email', 'You must enter a valid email address').isEmail();
+    req.assert('password', 'Password must be between 8-20 characters long').len(8, 20);
+    req.assert('username', 'Username cannot be more than 128 characters').len(1,128);
+    req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
+
+    var errors = req.validationErrors();
+    if (errors) {
+        return res.status(400).send(errors);
+    }
+
+    // Hard coded for now. Will address this with the user permissions system in v0.3.5
+    user.roles = ['authenticated'];
+    user.ui_settings = [];
+    user.save(function(err) {
+        if (err) {
+            switch (err.code) {
+                case 11000:
+                case 11001:
+                    res.status(400).send('Username already taken');
+                    break;
+                default:
+                    res.status(400).send('Please fill all the required fields');
+            }
+            return res.status(400);
+        }
+        res.status(200);
+    });
+    res.jsonp(user);
+};
+
 /**
  * Send User
  */
