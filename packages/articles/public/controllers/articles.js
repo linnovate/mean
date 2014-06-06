43734c06d190e178413f7acd40458b9600544ee9
Keith <keith@keithfimreite.com>
2014-06-05 21:33:42 -0700

    Validation Update

--- packages/articles/public/controllers/articles.js
+++ packages/articles/public/controllers/articles.js
@@ -9,17 +9,21 @@ angular.module('mean').controller('ArticlesController', ['$scope', '$stateParams
             return $scope.global.isAdmin || article.user._id === $scope.global.user._id;
         };
 
-        $scope.create = function() {
-            var article = new Articles({
-                title: this.title,
-                content: this.content
-            });
-            article.$save(function(response) {
-                $location.path('articles/' + response._id);
-            });
+        $scope.create = function(isValid) {
+            if (isValid){
+                var article = new Articles({
+                    title: this.title,
+                    content: this.content
+                });
+                article.$save(function(response) {
+                    $location.path('articles/' + response._id);
+                });
 
-            this.title = '';
-            this.content = '';
+                this.title = '';
+                this.content = '';
+            }else{
+                $scope.submitted = true;
+            }
         };
 
         $scope.remove = function(article) {
@@ -38,16 +42,20 @@ angular.module('mean').controller('ArticlesController', ['$scope', '$stateParams
             }
         };
 
-        $scope.update = function() {
-            var article = $scope.article;
-            if (!article.updated) {
-                article.updated = [];
-            }
-            article.updated.push(new Date().getTime());
+        $scope.update = function(isValid) {
+            if (isValid){
+                var article = $scope.article;
+                if (!article.updated) {
+                    article.updated = [];
+                }
+                article.updated.push(new Date().getTime());
 
-            article.$update(function() {
-                $location.path('articles/' + article._id);
-            });
+                article.$update(function() {
+                    $location.path('articles/' + article._id);
+                });
+            }else{
+                $scope.submitted = true;
+            }
         };
 
         $scope.find = function() {
