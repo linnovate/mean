8c7d74ad697933373c450b693348f00091e85baa
Fouad Kada <fouad.kada@gmail.com>
2014-06-17 23:14:11 +0300

    adding a condition to check whether a menu is an array and if not initialize a new array and add to it the single element menu...when menus contain only one element they are an object instead of an array so the forEach loop will fail and the menu will not display at all

--- server/routes/menus.js
+++ server/routes/menus.js
@@ -2,17 +2,23 @@
 
 var mean = require('meanio');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
     app.route('/admin/menu/:name')
-        .get(function(req, res) {
+        .get(function (req, res) {
             var roles = (req.user ? req.user.roles : ['anonymous']);
             var menu = req.params.name ? req.params.name : 'main';
             var defaultMenu = (req.query.defaultMenu ? req.query.defaultMenu : []);
 
-            defaultMenu.forEach(function(item, index) {
-                defaultMenu[index] = JSON.parse(item);
-            });
+            if (Object.prototype.toString.call(defaultMenu) === '[object Array]') {
+                defaultMenu.forEach(function (item, index) {
+                    defaultMenu[index] = JSON.parse(item);
+                });
+            } else {
+                var temp = [];
+                temp.push(JSON.parse(defaultMenu));
+                defaultMenu = temp;
+            }
 
             var items = mean.menus.get({
                 roles: roles,
