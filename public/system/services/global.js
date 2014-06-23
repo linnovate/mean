fb0abb3ee54077bfea6066aed860aeefcb0e8f82
Keith <keith@keithfimreite.com>
2014-06-22 21:39:33 -0700

    incorrect logic for isAdmin
    I was failing authentication this "~window.user.roles.indexOf('admin');" seems completely incorrect. 
    ->Negate the index of ('admin') I kept getting -1 for the isAdmin function and it should have been true. Or am I missing something here?

--- public/system/services/global.js
+++ public/system/services/global.js
@@ -12,7 +12,7 @@ angular.module('mean.system').factory('Global', [
         };
         if (window.user && window.user.roles) {
             _this._data.authenticated = window.user.roles.length;
-            _this._data.isAdmin = ~window.user.roles.indexOf('admin');
+            _this._data.isAdmin = window.user.roles.indexOf('admin') > -1;
         }
         return _this._data;
     }
