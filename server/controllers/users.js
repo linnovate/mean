bf91ff49eba4edeb04a23b2aa33312730f0b2858
Liran Tal <liran.tal@gmail.com>
2014-06-30 01:14:12 +0300

    fixed pre save validations to return error objects as angular expects them. fixes also validation error for 'username already taken' which was not showing up to the user previously

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -65,10 +65,26 @@ exports.create = function(req, res, next) {
             switch (err.code) {
                 case 11000:
                 case 11001:
-                    res.status(400).send('Username already taken');
+                    res.status(400).send([{
+                        msg: 'Username already taken',
+                        param: 'username'
+                    }]);
                     break;
                 default:
-                    res.status(400).send('Please fill all the required fields');
+                    var modelErrors = [];
+
+                    if (err.errors) {
+
+                        for (var x in err.errors) {
+                            modelErrors.push({
+                                param: x,
+                                msg: err.errors[x].message,
+                                value: err.errors[x].value
+                            });
+                        }
+
+                        res.status(400).send(modelErrors);
+                    }
             }
 
             return res.status(400);
