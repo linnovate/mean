7aaefda1d7fbb1d765e8263b6206a6e7e7f04a3d
Drew Fyock <fyockm@gmail.com>
2014-06-30 17:13:22 -0400

    create unit test for menus

---
+++ test/lib/mean_test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var mean = require('../..'),
+    should = require('should');
+
+describe('mean.js', function() {
+
+    it('exports an object', function() {
+        should.exist(mean);
+        mean.should.be.an.Object;
+    });
+
+    var menus;
+    before(function() {
+        menus = new mean.Menus();
+    });
+    it('creates an empty menu array', function() {
+        menus.get().should.be.an.Array.and.be.empty;
+    });
+    it('adds a menu', function() {
+        menus.add({
+            title: 'test'
+        });
+        menus.get().should.eql([{
+            title: 'test',
+            menu: 'main',
+            roles: ['anonymous']
+        }]);
+    });
+    it('adds 2 menus', function() {
+        menus.get().should.be.an.Array.and.have.length(1); // 1 anonymous
+        menus.add({
+            title: 'auth',
+            roles: ['authenticated']
+        }, {
+            title: 'mocha',
+            roles: ['mocha']
+        });
+        menus.get({
+            roles: ['authenticated']
+        }).should.be.an.Array.and.have.length(2); // anonymous, authenticated
+    });
+
+});
