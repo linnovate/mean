034d57661081b7706a912a897f2a52932cebde70
Drew Fyock <fyockm@gmail.com>
2014-07-01 15:11:45 -0400

    reimplement 53375b6

--- config/express.js
+++ config/express.js
@@ -83,7 +83,9 @@ module.exports = function(app, passport, db) {
             collection: config.sessionCollection
         }),
         cookie: config.sessionCookie,
-        name: config.sessionName
+        name: config.sessionName,
+        resave: true,
+        saveUninitialized: true
     }));
 
     // Dynamic helpers
