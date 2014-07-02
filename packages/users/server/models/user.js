0609ecbe9ee9d5123b2d9bfd093ea0c9d3a02c4d
Liran Tal <liran.tal@gmail.com>
2014-07-02 12:32:28 +0300

    adding support to validate user email address, otherwise multiple users can register with the same email address

--- packages/users/server/models/user.js
+++ packages/users/server/models/user.js
@@ -15,6 +15,13 @@ var validatePresenceOf = function(value) {
     return (this.provider && this.provider !== 'local') || (value && value.length);
 };
 
+var validateUniqueEmail = function(value, callback) {
+    var User = mongoose.model('User');
+    User.find({email : value}, function(err, user) {
+        callback(err || user.length === 0);
+    });
+};
+
 /**
  * User Schema
  */
@@ -26,7 +33,8 @@ var UserSchema = new Schema({
     email: {
         type: String,
         required: true,
-        match: [/.+\@.+\..+/, 'Please enter a valid email']
+        match: [/.+\@.+\..+/, 'Please enter a valid email'],
+        validate: [validateUniqueEmail, 'E-mail address is already in-use']
     },
     username: {
         type: String,
