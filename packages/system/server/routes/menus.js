f1efb7f0534b1b9be188ea8dc75ca708e5d90cd5
Drew Fyock <fyockm@gmail.com>
2014-07-09 09:38:49 -0400

    fix #605 allow for single menu object instead of array

--- packages/system/server/routes/menus.js
+++ packages/system/server/routes/menus.js
@@ -6,21 +6,20 @@ module.exports = function(System, app, auth, database) {
 
     app.route('/admin/menu/:name')
         .get(function(req, res) {
-            var roles = (req.user ? req.user.roles : ['anonymous']);
-            var menu = req.params.name ? req.params.name : 'main';
-            var defaultMenu = (req.query.defaultMenu ? req.query.defaultMenu : []);
+            var roles = req.user ? req.user.roles : ['anonymous'];
+            var menu = req.params.name || 'main';
+            var defaultMenu = req.query.defaultMenu || [];
 
-            defaultMenu.forEach(function(item, index) {
-                defaultMenu[index] = JSON.parse(item);
-            });
+            if (!Array.isArray(defaultMenu)) defaultMenu = [defaultMenu];
 
             var items = mean.menus.get({
                 roles: roles,
                 menu: menu,
-                defaultMenu: defaultMenu
+                defaultMenu: defaultMenu.map(function(item) {
+                    return JSON.parse(item);
+                })
             });
 
             res.json(items);
         });
-
 };
