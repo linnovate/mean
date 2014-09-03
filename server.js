10709c69bb1468fbff843f78e3b6aea0c9a6ece2
Drew Fyock <fyockm@gmail.com>
2014-09-03 14:54:15 -0400

    fix server start message for https

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ var mean = require('meanio');
 
 // Creates and serves mean application
 mean.serve({ /*options placeholder*/ }, function(app, config) {
-  console.log('Mean app started on port ' + config.http.port + ' (' + process.env.NODE_ENV + ')');
-  if(config.https && config.https.port){
-    console.log('Mean secure app started on port ' + config.https.port + ' (' + process.env.NODE_ENV + ')');
-  }
+	var port = config.https && config.https.port ? config.https.port : config.http.port;
+	console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ')');
 });
