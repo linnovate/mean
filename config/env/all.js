0ef2179a2da677669965021deb75316ae1bde362
Christopher Chang <chris@mongolab.com>
2014-09-03 10:44:21 -0700

    Update all.js
    use MongoLab URL, if exists

--- config/env/all.js
+++ config/env/all.js
@@ -18,7 +18,7 @@ module.exports = {
     }
   },
   hostname: process.env.HOST || process.env.HOSTNAME,
-  db: process.env.MONGOHQ_URL,
+  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URL,
   templateEngine: 'swig',
 
   // The secret should be set to a non-guessable string that
