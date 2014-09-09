7b1d966bcf0a60a513546dee65d1d492e44fd4f0
Lior Kesos <lior@linnovate.net>
2014-09-09 08:53:45 +0300

    Fixes #739, @enkodellc solved it by upgrading nodemailer to 1.2.2 and changing config in transport

--- config/env/development.js
+++ config/env/development.js
@@ -35,7 +35,7 @@ module.exports = {
   },
   emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
   mailer: {
-    service: 'SERVICE_PROVIDER',
+    service: 'SERVICE_PROVIDER', // Gmail, SMTP
     auth: {
       user: 'EMAIL_ID',
       pass: 'PASSWORD'
