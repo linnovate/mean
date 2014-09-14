a1366e4d6f01964db8af1d7d9a821c9e6fe13b39
Lior Kesos <lior@linnovate.net>
2014-09-15 00:46:49 +0300

    Adding theme state visibility - currently quirky - will debug with team tommorow

--- packages/theme/public/controllers/theme.js
+++ packages/theme/public/controllers/theme.js
@@ -1,10 +1,22 @@
 'use strict';
 
-angular.module('mean.theme').controller('ThemeController', ['$scope', 'Global', 'Theme',
-  function($scope, Global, Theme) {
-    $scope.global = Global;
-    $scope.package = {
-      name: 'theme'
-    };
-  }
-]);
+angular.module('mean.theme')
+	.controller('ThemeController', ['$scope', 'Global', '$location', '$rootScope',
+	  function($scope, Global, $location, $rootScope) {
+	 		$rootScope.$on('$stateChangeStart',
+      	function(event, toState, toParams, fromState, fromParams){
+          var elements = $location.path().split('/');
+          var lastElement = elements.slice(-1)[0];
+        	$scope.state = toState;
+        	if($scope.state === '' ) {
+          	$scope.state = 'firstPage';
+        	}
+        	console.log($scope.state);
+    		});
+// Original scaffolded code.
+      $scope.global = Global;
+      $scope.package = {
+        name: 'theme'
+      };
+    }
+  ]);
