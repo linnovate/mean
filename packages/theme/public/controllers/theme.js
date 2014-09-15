1c6b9458b2e05f85fda81906f1b3c5c5dd8e7382
Lior Kesos <lior@linnovate.net>
2014-09-15 16:53:14 +0300

    Added a class for state in the body tag and using it in the theme for selective backgrounds (only for the login/register/password stuff)

--- packages/theme/public/controllers/theme.js
+++ packages/theme/public/controllers/theme.js
@@ -5,13 +5,11 @@ angular.module('mean.theme')
 	  function($scope, Global, $location, $rootScope) {
 	 		$rootScope.$on('$stateChangeStart',
       	function(event, toState, toParams, fromState, fromParams){
-          var elements = $location.path().split('/');
-          var lastElement = elements.slice(-1)[0];
-        	$scope.state = toState;
+					var toPath = toState.url.replace('/','');
+        	$scope.state = toPath;
         	if($scope.state === '' ) {
           	$scope.state = 'firstPage';
         	}
-        	console.log($scope.state);
     		});
 // Original scaffolded code.
       $scope.global = Global;
