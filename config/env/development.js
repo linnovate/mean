f3e88a975d34a4330a6da7b35d0167c18e8ae4a0
Lior Kesos <lior@linnovate.net>
2014-09-16 13:47:27 +0300

    updated teh different DEFAULT_ prefixes

--- config/env/development.js
+++ config/env/development.js
@@ -9,27 +9,27 @@ module.exports = {
     name: 'MEAN - FullStack JS - Development'
   },
   facebook: {
-    clientID: 'APP_ID',
+    clientID: 'DEFAULT_APP_ID',
     clientSecret: 'APP_SECRET',
     callbackURL: 'http://localhost:3000/auth/facebook/callback'
   },
   twitter: {
-    clientID: 'CONSUMER_KEY',
+    clientID: 'DEFAULT_CONSUMER_KEY',
     clientSecret: 'CONSUMER_SECRET',
     callbackURL: 'http://localhost:3000/auth/twitter/callback'
   },
   github: {
-    clientID: 'APP_ID',
+    clientID: 'DEFAULT_APP_ID',
     clientSecret: 'APP_SECRET',
     callbackURL: 'http://localhost:3000/auth/github/callback'
   },
   google: {
-    clientID: 'APP_ID',
+    clientID: 'DEFAULT_APP_ID',
     clientSecret: 'APP_SECRET',
     callbackURL: 'http://localhost:3000/auth/google/callback'
   },
   linkedin: {
-    clientID: 'API_KEY',
+    clientID: 'DEFAULY_API_KEY',
     clientSecret: 'SECRET_KEY',
     callbackURL: 'http://localhost:3000/auth/linkedin/callback'
   },
