02bde8a89dd2b896a540bcbfa881e67916907a25
greenlikeorange <beginofalove@hotmail.com>
2014-10-03 03:12:28 +0630

    Authorized with specific roles

--- packages/articles/server/routes/articles.js
+++ packages/articles/server/routes/articles.js
@@ -9,8 +9,8 @@ module.exports = function(Articles, app, auth) {
     .post(auth.requiresLogin, articles.create);
   app.route('/articles/:articleId')
     .get(articles.show)
-    .put(auth.requiresLogin, hasAuthorization(['admin', 'authenticated']), articles.update)
-    .delete(auth.requiresLogin, hasAuthorization(['admin', 'authenticated']), articles.destroy);
+    .put(auth.requiresLogin, articles.hasAuthorization(['admin', 'authenticated']), articles.update)
+    .delete(auth.requiresLogin, articles.hasAuthorization(['admin', 'authenticated']), articles.destroy);
 
   // Finish with setting up the articleId param
   app.param('articleId', articles.article);
