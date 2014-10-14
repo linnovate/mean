b7cbbc47b1a95f05f7595fd203f64da23bcf25dd
Lior Kesos <lior@linnovate.net>
2014-10-14 13:00:54 +0300

    More 1st page progress

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -3,5 +3,58 @@
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
   function($scope, Global) {
     $scope.global = Global;
+    $scope.sites = {
+      'makeapoint':{
+        'name':'makeapoint',
+        'text':'Makeapoint is a platform to craft and fine-tune ideas and
+                messages providing a graphical experience which brough an
+                offline methodlogy online',
+        'author':'Linnovate',
+        'link':'http://www.linnovate.net',
+        'image':'http://cdn.designbyhumans.com/pictures/blog/09-2013/pop-culture-cats/Pop_Culture_Cats_Hamilton_Hipster.jpg'
+      },
+      'intranet':{
+        'name':'Intranet',
+        'text':'Enterpintranet',
+        'author':'qed42',
+        'link':'http://www.qed42.com',
+        'image':'http://www.catster.com/files/cat-stretching-shutterstock_61984297.jpg'
+      }
+    }
+    $scope.packages = {
+      'gmap':{
+        'name':'Gmap',
+        'text':'hello text',
+        'author':'Linnovate',
+        'link':'http://www.linnovate.net',
+        'image':'http://cdn.designbyhumans.com/pictures/blog/09-2013/pop-culture-cats/Pop_Culture_Cats_Hamilton_Hipster.jpg'
+      },
+      'upload':{
+        'name':'Upload',
+        'text':'hello text',
+        'author':'Linnovate',
+        'link':'http://www.linnovate.net',
+        'image':'http://cdn.designbyhumans.com/pictures/blog/09-2013/pop-culture-cats/Pop_Culture_Cats_Hamilton_Hipster.jpg'
+      },
+      'socket':{
+        'name':'Socket',
+        'text':'Socket.io support',
+        'author':'Linnovate',
+        'link':'http://www.linnovate.net',
+        'image':'http://cdn.designbyhumans.com/pictures/blog/09-2013/pop-culture-cats/Pop_Culture_Cats_Hamilton_Hipster.jpg'
+      }
+    }
+
+    $scope.$watch(function () {
+      for (var i = 0; i < $scope.sites.length; i++) {
+        if ($scope.sites[i].active) {
+          return $scope.sites[i];
+        }
+      }
+    }, function (currentSlide, previousSlide) {
+      if (currentSlide !== previousSlide) {
+        console.log('currentSlide:', currentSlide);
+      }
+    });
   }
 ]);
