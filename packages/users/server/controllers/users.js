9562b778f8283727e94a6c34af72503d5b98d3fb
Emil Kantis <e.kantis@gmail.com>
2014-10-17 22:42:33 +0200

    Reduced diff again

--- packages/users/server/controllers/users.js
+++ packages/users/server/controllers/users.js
@@ -57,6 +57,7 @@ exports.create = function(req, res, next) {
   req.assert('password', 'Password must be between 8-20 characters long').len(8, 20);
   req.assert('username', 'Username cannot be more than 20 characters').len(1, 20);
   req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
+  
   var errors = req.validationErrors();
 
   if (!req.session.captcha.valid) {
