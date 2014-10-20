69a58a8a8725ae396e29464b6d1caa86ac3f1ab6
Drew Fyock <fyockm@gmail.com>
2014-10-20 11:26:01 -0400

    fix preinstall script

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,21 +1,28 @@
-var args = process.argv.slice(2);
+'use strict';
 
-if (~args.indexOf('meanio') && (~args.indexOf('-g') || ~args.indexOf('--global'))) {
+var args = process.argv;
+if (process.env.npm_config_argv) {
+  try {
+    args = JSON.parse(process.env.npm_config_argv).cooked;
+  } catch (e) {}
+}
+
+if (~args.indexOf('meanio') && ~args.indexOf('--global')) {
   console.log('Please install \'mean-cli\' globally instead of \'meanio\' as:');
   console.log('');
   console.log('  $ npm install -g mean-cli');
   console.log('');
   process.exit(1);
 } else if (~args.indexOf('mean-cli')) {
-  var spawn = require('child_process').spawn;
-  npmls = spawn('npm', ['ls', '--global', '--parseable', '--depth', '0']),
-  grep = spawn('grep', ['meanio']);
+  var spawn = require('child_process').spawn,
+    npmls = spawn('npm', ['ls', '--global', '--parseable', '--depth', '0']),
+    grep = spawn('grep', ['meanio']);
 
   npmls.stdout.on('data', function(data) {
     grep.stdin.write(data);
   });
 
-  npmls.on('close', function(code) {
+  npmls.on('close', function() {
     grep.stdin.end();
   });
 
