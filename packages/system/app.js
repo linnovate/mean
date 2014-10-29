6864d0f4315c2fcb5ef9deed39abc7a327c2993b
Yonatan Ellman <yonatan@nodeside.com>
2014-10-29 17:20:26 +0200

    quick fix to work around linting issue due to System being previouslly declared

--- packages/system/app.js
+++ packages/system/app.js
@@ -7,18 +7,18 @@ var Module = require('meanio').Module,
   favicon = require('serve-favicon'),
   express = require('express');
 
-var System = new Module('system');
+var SystemPackage = new Module('system');
 
 /*
  * All MEAN packages require registration
  * Dependency injection is used to define required modules
  */
-System.register(function(app, auth, database) {
+SystemPackage.register(function(app, auth, database) {
 
   //We enable routing. By default the Package Object is passed to the routes
-  System.routes(app, auth, database);
+  SystemPackage.routes(app, auth, database);
 
-  System.aggregateAsset('css', 'common.css');
+  SystemPackage.aggregateAsset('css', 'common.css');
 
   // The middleware in config/express will run before this code
 
@@ -31,5 +31,5 @@ System.register(function(app, auth, database) {
   // Adding robots and humans txt
   app.use(express.static(__dirname + '/public/assets/static'));
 
-  return System;
+  return SystemPackage;
 });
