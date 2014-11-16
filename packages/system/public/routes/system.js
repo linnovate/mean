6e5c5c6c6c97cd00bc64f8b496068614fe106932
Robert Misiak <robertm702@gmail.com>
2014-11-16 00:23:07 -0800

    incorporating feedback on code organization.

--- packages/system/public/routes/system.js
+++ packages/system/public/routes/system.js
@@ -1,5 +1,46 @@
 'use strict';
 
+// $viewPathProvider, to allow overriding system default views
+angular.module('mean.system').provider('$viewPath', function() {
+  function ViewPathProvider() {
+    var overrides = {};
+
+    this.path = function(path) {
+      return function() {
+        return overrides[path] || path;
+      };
+    };
+
+    this.override = function(defaultPath, newPath) {
+      if (overrides[defaultPath]) {
+        throw new Error('View already has an override: ' + defaultPath);
+      }
+      overrides[defaultPath] = newPath;
+    };
+
+    this.$get = function() {
+      return this;
+    };
+  }
+
+  return new ViewPathProvider();
+});
+
+// $meanStateProvider, provider to wire up $viewPathProvider to $stateProvider
+angular.module('mean.system').provider('$meanState', ['$stateProvider', '$viewPathProvider', function($stateProvider, $viewPathProvider) {
+  this.state = function(stateName, data) {
+    if (data.templateUrl) {
+      data.templateUrl = $viewPathProvider.path(data.templateUrl);
+    }
+    $stateProvider.state(stateName, data);
+    return this;
+  };
+
+  this.$get = function() {
+    return this;
+  };
+}]);
+
 //Setting up route
 angular.module('mean.system').config(['$meanStateProvider', '$urlRouterProvider',
   function($meanStateProvider, $urlRouterProvider) {
