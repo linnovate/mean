145aa39dd5519092bbdf538a765f5477b527f706
Keith <keith@keithfimreite.com>
2014-11-24 03:18:00 -0700

    Express update
    deprecate res.json(status, obj) -- use res.status(status).json(obj) instead
    https://github.com/strongloop/express/blob/master/History.md#470--2014-07-25

--- packages/access/server/config/authorization.js
+++ packages/access/server/config/authorization.js
@@ -5,7 +5,7 @@
  */
 exports.requiresLogin = function(req, res, next) {
   if (!req.isAuthenticated()) {
-    return res.send(401, 'User is not authorized');
+    return res.status(401).send('User is not authorized');
   }
   next();
 };
@@ -16,7 +16,7 @@ exports.requiresLogin = function(req, res, next) {
  */
 exports.requiresAdmin = function(req, res, next) {
   if (!req.isAuthenticated() || !req.user.hasRole('admin')) {
-    return res.send(401, 'User is not authorized');
+    return res.status(401).send('User is not authorized');
   }
   next();
 };
