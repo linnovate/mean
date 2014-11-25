3674bb666295d3a9991e125983f3be865da49b0a
lugithub <lu.bin.lu.bin@gmail.com>
2014-11-25 10:27:18 -0500

    fix formatting for system public tests interceptor.spec.js

--- packages/system/public/tests/interceptor.spec.js
+++ packages/system/public/tests/interceptor.spec.js
@@ -2,74 +2,62 @@
 
 (function () {
 
-    var httpProviderIt;
-    var _httpInterceptor;
+  var httpProviderIt;
+  var _httpInterceptor;
+  var rejection = {status: 401};
 
-    describe('system public services', function () {
-        describe('httpInterceptor', function () {
+  describe('system public services', function () {
+    describe('httpInterceptor', function () {
 
-            beforeEach(function () {
-
-                module('mean-factory-interceptor');
-
-                module('mean-factory-interceptor', function ($httpProvider) {
-                    httpProviderIt = $httpProvider;
-                });
-
-
-                inject(function (httpInterceptor) {
-                    _httpInterceptor = httpInterceptor;
-                });
-            });
-
-            // check to see if it has the expected function
-            it('should exist', function () {
-                expect(_httpInterceptor).toBeDefined();
-            });
-
-            it('should be contained as an interceptor', function () {
-                expect(httpProviderIt.interceptors).toContain('httpInterceptor');
-            });
-
-            it('should have an empty response', function () {
-                var response = {};
-                expect(_httpInterceptor.response(response)).toBe(response);
-            });
-
-            it('should reject by response', function (done) {
-
-                inject(function ($q, $rootScope) {
-                    var rejection = {status: 401};
-
-                    _httpInterceptor.response(rejection).then(function () {
-                        },
-                        function (reason) {
-                            expect(reason).toBe(rejection);
-                            done();
-                        });
-
-                    $rootScope.$apply();
-                });
-                
-            });
-
-            it('should reject by responseError', function (done) {
-
-                inject(function ($q, $rootScope) {
-                        var rejection = {status: 401};
-
-                        _httpInterceptor.responseError(rejection).then(function () {
-                            },
-                            function (reason) {
-                                expect(reason).toBe(rejection);
-                                done();
-                            });
-
-                        $rootScope.$apply();
-                    }
-                );
-            });
-
+      beforeEach(function () {
+        module('mean-factory-interceptor');
+        module('mean-factory-interceptor', function ($httpProvider) {
+          httpProviderIt = $httpProvider;
         });
+
+        inject(function (httpInterceptor) {
+          _httpInterceptor = httpInterceptor;
+        });
+      });
+
+      it('should exist', function () {
+        expect(_httpInterceptor).toBeDefined();
+      });
+
+      it('should be contained as an interceptor', function () {
+        expect(httpProviderIt.interceptors).toContain('httpInterceptor');
+      });
+
+      it('should have an empty response', function () {
+        var response = {};
+        expect(_httpInterceptor.response(response)).toBe(response);
+      });
+
+      it('should reject by response', function (done) {
+        inject(function ($rootScope) {
+          _httpInterceptor.response(rejection).then(function () {
+            },
+            function (reason) {
+              expect(reason).toBe(rejection);
+              done();
+            });
+
+          $rootScope.$apply();
+        });
+      });
+
+      it('should reject by responseError', function (done) {
+        inject(function ($rootScope) {
+          _httpInterceptor.responseError(rejection).then(function () {
+            },
+            function (reason) {
+              expect(reason).toBe(rejection);
+              done();
+            });
+
+          $rootScope.$apply();
+        });
+      });
     });
+  });
 }());
