6e09a3fdcf6ccd4640849cc1ebfe2311306eba5f
Enkode <keith@keithfimreite.com>
2014-11-25 14:47:43 -0700

    Update Express req.status, validate MongoId parameters

--- packages/access/server/config/authorization.js
+++ packages/access/server/config/authorization.js
@@ -1,11 +1,13 @@
 'use strict';
+var mongoose = require('mongoose'),
+  _ = require('lodash');
 
 /**
  * Generic require login routing middleware
  */
 exports.requiresLogin = function(req, res, next) {
   if (!req.isAuthenticated()) {
-    return res.send(401, 'User is not authorized');
+    return res.status(401).send('User is not authorized');
   }
   next();
 };
@@ -16,7 +18,19 @@ exports.requiresLogin = function(req, res, next) {
  */
 exports.requiresAdmin = function(req, res, next) {
   if (!req.isAuthenticated() || !req.user.hasRole('admin')) {
-    return res.send(401, 'User is not authorized');
+    return res.status(401).send('User is not authorized');
+  }
+  next();
+};
+
+/**
+ * Generic validates if the first parameter is a mongo ObjectId
+ */
+exports.isMongoId = function(req, res, next) {
+  if (_.size(req.params) === 1) {
+    if (!mongoose.Types.ObjectId.isValid(_.values(req.params)[0])) {
+      return res.status(500).send('Parameter passed is not a valid Mongo ObjectId');
+    }
   }
   next();
 };
