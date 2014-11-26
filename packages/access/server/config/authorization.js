6ceeba3816994fce6956def875d34174d80669dc
Keith <keith@keithfimreite.com>
2014-11-25 21:31:38 -0700

    Update authorization.js

--- packages/access/server/config/authorization.js
+++ packages/access/server/config/authorization.js
@@ -27,8 +27,7 @@ exports.requiresAdmin = function(req, res, next) {
  * Generic validates if the first parameter is a mongo ObjectId
  */
 exports.isMongoId = function(req, res, next) {
-  if (_.size(req.params) === 1) {
-    if (!mongoose.Types.ObjectId.isValid(_.values(req.params)[0])) {
+  if ((_.size(req.params) === 1) && (!mongoose.Types.ObjectId.isValid(_.values(req.params)[0]))) {
       return res.status(500).send('Parameter passed is not a valid Mongo ObjectId');
     }
   }
