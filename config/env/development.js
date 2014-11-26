d4b4ad0cd32355cce8a79e5ceb6a9214a349a0db
Erick Tampubolon <lontongcorp@gmail.com>
2014-11-26 15:07:16 +0700

    Update development.js
    config.social group

--- config/env/development.js
+++ config/env/development.js
@@ -9,31 +9,39 @@ module.exports = {
   app: {
     name: 'MEAN - FullStack JS - Development'
   },
-  facebook: {
-    clientID: 'DEFAULT_APP_ID',
-    clientSecret: 'APP_SECRET',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
-  },
-  twitter: {
-    clientID: 'DEFAULT_CONSUMER_KEY',
-    clientSecret: 'CONSUMER_SECRET',
-    callbackURL: 'http://localhost:3000/auth/twitter/callback'
-  },
-  github: {
-    clientID: 'DEFAULT_APP_ID',
-    clientSecret: 'APP_SECRET',
-    callbackURL: 'http://localhost:3000/auth/github/callback'
-  },
-  google: {
-    clientID: 'DEFAULT_APP_ID',
-    clientSecret: 'APP_SECRET',
-    callbackURL: 'http://localhost:3000/auth/google/callback'
-  },
-  linkedin: {
-    clientID: 'DEFAULT_API_KEY',
-    clientSecret: 'SECRET_KEY',
-    callbackURL: 'http://localhost:3000/auth/linkedin/callback'
-  },
+  //social: {
+    facebook: {
+      clientID: 'DEFAULT_APP_ID',
+      clientSecret: 'APP_SECRET',
+      //scope: ['email', 'user_about_me'],
+      callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    },
+    twitter: {
+      clientID: 'DEFAULT_CONSUMER_KEY',
+      clientSecret: 'CONSUMER_SECRET',
+      callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    },
+    github: {
+      clientID: 'DEFAULT_APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    google: {
+      clientID: 'DEFAULT_APP_ID',
+      clientSecret: 'APP_SECRET',
+      //scope: [
+      //  'https://www.googleapis.com/auth/userinfo.profile',
+      //  'https://www.googleapis.com/auth/userinfo.email'
+      //],
+      callbackURL: 'http://localhost:3000/auth/google/callback'
+    },
+    linkedin: {
+      clientID: 'DEFAULT_API_KEY',
+      clientSecret: 'SECRET_KEY',
+      //scope: ['r_emailaddress'],
+      callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    },
+  //},
   emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
   mailer: {
     service: 'SERVICE_PROVIDER', // Gmail, SMTP
