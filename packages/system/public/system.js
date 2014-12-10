13a3cdc115175930078e109104836cd193e499d7
AlmogBaku <almog.baku@gmail.com>
2014-12-10 15:04:55 +0200

    fixes the body state class for urls-with-params

--- packages/system/public/system.js
+++ packages/system/public/system.js
@@ -1,3 +1,14 @@
 'use strict';
 
-angular.module('mean.system', ['ui.router', 'mean-factory-interceptor']);
+angular.module('mean.system', ['ui.router', 'mean-factory-interceptor'])
+  .run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+      var toPath = toState.url.replace('/','');
+      toPath = toPath.replace(':','-');
+      $rootScope.state = toPath;
+      if($rootScope.state === '' ) {
+        $rootScope.state = 'firstPage';
+      }
+    });
+  }])
+;
--- packages/theme/public/controllers/theme.js
+++ packages/theme/public/controllers/theme.js
@@ -1,16 +1,8 @@
 'use strict';
 
 angular.module('mean.theme')
-	.controller('ThemeController', ['$scope', 'Global', '$location', '$rootScope',
-	  function($scope, Global, $location, $rootScope) {
-	 		$rootScope.$on('$stateChangeStart',
-      	function(event, toState, toParams, fromState, fromParams){
-					var toPath = toState.url.replace('/','');
-        	$scope.state = toPath;
-        	if($scope.state === '' ) {
-          	$scope.state = 'firstPage';
-        	}
-    		});
+	.controller('ThemeController', ['$scope', 'Global',
+	  function($scope, Global) {
 // Original scaffolded code.
       $scope.global = Global;
       $scope.package = {
