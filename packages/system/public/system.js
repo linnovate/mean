12eb7595ee9d9d831e840e189270ebb619658e0c
AlmogBaku <almog.baku@gmail.com>
2014-12-14 18:02:08 +0200

    fix global replace

--- packages/system/public/system.js
+++ packages/system/public/system.js
@@ -3,8 +3,10 @@
 angular.module('mean.system', ['ui.router', 'mean-factory-interceptor'])
   .run(['$rootScope', function($rootScope) {
     $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
-      var toPath = toState.url.replace('/','');
-      toPath = toPath.replace(':','-');
+      var toPath = toState.url;
+      toPath = toPath.replace(new RegExp('/', 'g'), '');
+      toPath = toPath.replace(new RegExp(':', 'g'),'-');
+      console.log(toState.url,toPath);
       $rootScope.state = toPath;
       if($rootScope.state === '' ) {
         $rootScope.state = 'firstPage';
