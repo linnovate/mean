f28bb797a20d513c4678a40615c5bc9e9c9d56a3
brownman <brownman556@gmail.com>
2014-12-17 17:02:14 +0200

    fix typo

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@
 
 module.exports = {
  // db: 'mongodb://localhost/mean-dev',
-  db: 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
   debug: true,
 //  aggregate: 'whatever that is not false, because boolean false value turns aggregation off', //false
   aggregate: true,
