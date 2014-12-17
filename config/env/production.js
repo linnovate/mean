1323559f301ee6512be52f2157ac8dd6eecabe25
brownman <brownman556@gmail.com>
2014-12-17 17:07:19 +0200

    fix: detect mongo host in docker env
    env: production

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://localhost/mean-prod',
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
