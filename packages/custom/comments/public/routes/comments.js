1fcdc4ab60cbc330615bc34511f13d8326d8fcb3
Riten Vagadiya <riten.s.v@gmail.com>
2015-01-24 02:32:40 +0400

    Added a simple service - sometimes i just dont like factory

--- packages/custom/comments/public/routes/comments.js
+++ packages/custom/comments/public/routes/comments.js
@@ -2,9 +2,6 @@
 
 angular.module('mean.comments').config(['$stateProvider',
   function($stateProvider) {
-    $stateProvider.state('comments example page', {
-      url: '/comments/example',
-      templateUrl: 'comments/views/index.html'
-    });
+    //so no routing needed for now
   }
 ]);
--- packages/custom/comments/public/services/comments.js
+++ packages/custom/comments/public/services/comments.js
@@ -1,9 +1,16 @@
 'use strict';
 
-angular.module('mean.comments').factory('Comments', [
-  function() {
-    return {
-      name: 'comments'
-    };
+angular.module('mean.comments').service('Comments', ['$resource',
+  function($resource) {
+	  this.single = $resource('comments/:commentId', {
+      commentId: '@_id'
+    }, {
+      update: {
+        method: 'PUT'
+      }
+    });
+    this.list = $resource('comments/list/:articleId', {
+      articleId: '@_id'
+    });
   }
 ]);
