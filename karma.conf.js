765ca64ae440e448118912270ebf61bb0d40b944
brownman <brownman556@gmail.com>
2015-02-24 13:37:37 +0200

    [karma] - look for server on port 3001

--- karma.conf.js
+++ karma.conf.js
@@ -44,6 +44,10 @@ module.exports = function(config) {
     // web server port
     port: 9876,
 
+    proxies: {
+      '/': 'http://localhost:3001/'
+    },
+
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
