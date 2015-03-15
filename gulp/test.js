73ed49c567b7ff4c7e6421642fc161b9b0b0293a
Lior Kesos <lior@linnovate.net>
2015-03-15 16:12:42 +0200

    gulp/test.js

--- gulp/test.js
+++ gulp/test.js
@@ -1,21 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  karma = require('karma').server;
 
 gulp.task('test', ['env:test', 'karma:unit', 'mochaTest']);
 
 gulp.task('karma:unit', function (done) {
   karma.start({
-    configFile: __dirname + '/karma.conf.js',
+    configFile: __dirname + '/../karma.conf.js',
     singleRun: true
   }, done);
 });
 
 gulp.task('loadTestSchema', function () {
-  require('server.js');
-  require('meanio/lib/util').preload(__dirname + '/packages/**/server', 'model');
+  require('../server.js');
+  require('../node_modules/meanio/lib/util').preload('../packages/**/server', 'model');
 });
 
 gulp.task('mochaTest', ['loadTestSchema'], function () {
-  return gulp.src('packages/**/server/tests/**/*.js', {read: false})
+  return gulp.src('../packages/**/server/tests/**/*.js', {read: false})
     .pipe(plugins.mocha({
       reporter: 'spec'
     }));
