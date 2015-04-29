833f35a35f3a288c41ede3b75cf23b7480c8c093
Orit Persik <orit@linnovate.net>
2015-04-29 09:04:33 +0300

    missing mean text

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -7,7 +7,7 @@ if (!shell.which('mean')) return missingMean();
 shell.exec('mean postinstall', function(code) {});
 
 function missingMean() {
-	console.log('you need to install preferences to packages by yourself,  press enter to continue');
+	console.log("You're attempting to deploy mean without the mean-cli, the mean post install script traverses through all packages and takes care of their dependencies, please install mean-cli and continue...");
 	process.stdin.resume();
 	process.stdin.on('data', function (text) {
 		process.exit();
