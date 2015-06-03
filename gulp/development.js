a79aba03484bc829aef27053a99cf10c307a900b
Zane Hitchcox <zwhitchcox@gmail.com>
2015-06-03 11:06:05 -0400

    add coffee

--- gulp/development.js
+++ gulp/development.js
@@ -5,16 +5,19 @@ var gulp = require('gulp'),
   through = require('through'),
   gutil = require('gulp-util'),
   plugins = gulpLoadPlugins(),
+  coffee = require('gulp-coffee'),
   paths = {
     js: ['*.js', 'test/**/*.js', '!test/coverage/**', '!bower_components/**', '!packages/**/node_modules/**', '!packages/contrib/**/*.js', '!packages/contrib/**/node_modules/**', '!packages/core/**/*.js', '!packages/core/public/assets/lib/**/*.js'],
     html: ['packages/**/public/**/views/**', 'packages/**/server/views/**'],
     css: ['!bower_components/**', 'packages/**/public/**/css/*.css', '!packages/contrib/**/public/**/css/*.css', '!packages/core/**/public/**/css/*.css'],
     less: ['**/public/**/css/*.less'],
-    sass: ['**/public/**/css/*.scss']
+    sass: ['**/public/**/css/*.scss'],
+    coffee: ['packages/**/public/**/*.coffee','*.coffee'],
+    coffees: ['packages/**/server/**/*.coffee']
   };
 
 /*var defaultTasks = ['clean', 'jshint', 'less', 'csslint', 'devServe', 'watch'];*/
-var defaultTasks = ['clean',  'less', 'csslint', 'devServe', 'watch'];
+var defaultTasks = ['coffee','clean',  'less', 'csslint', 'devServe', 'watch'];
 
 gulp.task('env:development', function () {
   process.env.NODE_ENV = 'development';
@@ -53,7 +56,15 @@ gulp.task('devServe', ['env:development'], function () {
   });
 });
 
+gulp.task('coffee', function() {
+  gulp.src(paths.coffee)
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./packages'))
+});
+
 gulp.task('watch', function () {
+  gulp.watch(paths.coffee,['coffee']).on('change', plugins.livereload.changed);
+  gulp.watch(paths.coffees).on('change',plugins.livereload.changed);
   gulp.watch(paths.js, ['jshint']).on('change', plugins.livereload.changed);
   gulp.watch(paths.html).on('change', plugins.livereload.changed);
   gulp.watch(paths.css, ['csslint']).on('change', plugins.livereload.changed);
