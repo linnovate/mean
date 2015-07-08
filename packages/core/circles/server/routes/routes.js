3a87c37ff8a8ed170c369f38efd6287bb904b2c7
Orit Persik <orit@linnovate.net>
2015-07-08 11:50:48 +0300

    protect several routes for admin

--- packages/core/circles/server/routes/routes.js
+++ packages/core/circles/server/routes/routes.js
@@ -15,11 +15,11 @@ module.exports = function(Circles, app, auth, database) {
     app.get('/api/circles/visualize', circles.visualize);
     app.get('/api/circles/tree', circles.tree);
     app.get('/api/circles/mine', circles.mine);
-    app.get('/api/circles/all', circles.all);
+    app.get('/api/circles/all', auth.requiresAdmin circles.all);
 
     app.route('/api/circles/:name')
         .post(auth.requiresAdmin, circles.create)
-        .put(circles.update)
+        .put(auth.requiresAdmin, circles.update)
         .get(circles.show);
 
 };
