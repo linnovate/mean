766aaa91ccc8e4aec1158556006886867ce073d5
Orit Persik <orit@linnovate.net>
2015-07-12 09:02:21 +0300

    rename dev db

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/meand1',
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
   debug: true,
   logging: {
     format: 'tiny'
