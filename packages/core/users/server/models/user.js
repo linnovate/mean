b649da9fb138a66c0b29a9451f14d56e303b43f0
enkodellc <keith@keithfimreite.com>
2015-11-20 01:11:16 -0700

    Breakout mongoose schema methods individually.

--- packages/core/users/server/models/user.js
+++ packages/core/users/server/models/user.js
@@ -4,9 +4,9 @@
  * Module dependencies.
  */
 var mongoose  = require('mongoose'),
-    Schema    = mongoose.Schema,
-    crypto    = require('crypto'),
-          _   = require('lodash');
+  Schema    = mongoose.Schema,
+  crypto    = require('crypto'),
+  _   = require('lodash');
 
 /**
  * Validations
@@ -108,75 +108,73 @@ UserSchema.pre('save', function(next) {
 /**
  * Methods
  */
-UserSchema.methods = {
 
-  /**
-   * HasRole - check if the user has required role
-   *
-   * @param {String} plainText
-   * @return {Boolean}
-   * @api public
-   */
-  hasRole: function(role) {
-    var roles = this.roles;
-    return roles.indexOf('admin') !== -1 || roles.indexOf(role) !== -1;
-  },
+/**
+ * HasRole - check if the user has required role
+ *
+ * @param {String} plainText
+ * @return {Boolean}
+ * @api public
+ */
+UserSchema.methods.hasRole = function(role) {
+  var roles = this.roles;
+  return roles.indexOf('admin') !== -1 || roles.indexOf(role) !== -1;
+};
 
-  /**
-   * IsAdmin - check if the user is an administrator
-   *
-   * @return {Boolean}
-   * @api public
-   */
-  isAdmin: function() {
-    return this.roles.indexOf('admin') !== -1;
-  },
+/**
+ * IsAdmin - check if the user is an administrator
+ *
+ * @return {Boolean}
+ * @api public
+ */
+UserSchema.methods.isAdmin = function() {
+  return this.roles.indexOf('admin') !== -1;
+};
 
-  /**
-   * Authenticate - check if the passwords are the same
-   *
-   * @param {String} plainText
-   * @return {Boolean}
-   * @api public
-   */
-  authenticate: function(plainText) {
-    return this.hashPassword(plainText) === this.hashed_password;
-  },
+/**
+ * Authenticate - check if the passwords are the same
+ *
+ * @param {String} plainText
+ * @return {Boolean}
+ * @api public
+ */
+UserSchema.methods.authenticate = function(plainText) {
+  return this.hashPassword(plainText) === this.hashed_password;
+};
 
-  /**
-   * Make salt
-   *
-   * @return {String}
-   * @api public
-   */
-  makeSalt: function() {
-    return crypto.randomBytes(16).toString('base64');
-  },
+/**
+ * Make salt
+ *
+ * @return {String}
+ * @api public
+ */
+UserSchema.methods.makeSalt = function() {
+  return crypto.randomBytes(16).toString('base64');
+};
 
-  /**
-   * Hash password
-   *
-   * @param {String} password
-   * @return {String}
-   * @api public
-   */
-  hashPassword: function(password) {
-    if (!password || !this.salt) return '';
-    var salt = new Buffer(this.salt, 'base64');
-    return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
-  },
+/**
+ * Hash password
+ *
+ * @param {String} password
+ * @return {String}
+ * @api public
+ */
+UserSchema.methods.hashPassword = function(password) {
+  if (!password || !this.salt) return '';
+  var salt = new Buffer(this.salt, 'base64');
+  return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+};
 
-  /**
-   * Hide security sensitive fields
-   * 
-   * @returns {*|Array|Binary|Object}
-   */
-  toJSON: function() {
-    var obj = this.toObject();
-    delete obj.hashed_password;
-    delete obj.salt;
-    return obj;
-  }
+/**
+ * Hide security sensitive fields
+ *
+ * @returns {*|Array|Binary|Object}
+ */
+UserSchema.methods.toJSON = function() {
+  var obj = this.toObject();
+  delete obj.hashed_password;
+  delete obj.salt;
+  return obj;
 };
 
 mongoose.model('User', UserSchema);
