f64ff78cd1faed54712e9f594a63e28e624a27e7
John Webb <vapesk@gmail.com>
2015-12-01 13:04:13 -0500

    Replaced resolves in admin and articles with circles checks

--- packages/core/admin/public/routes/admin.js
+++ packages/core/admin/public/routes/admin.js
@@ -1,51 +1,41 @@
 'use strict';
 angular.module('mean.admin').config(['$stateProvider', '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('users', {
-                url: '/admin/users',
-                templateUrl: 'admin/views/users.html',
-                resolve: {
-                  isAdmin: function(MeanUser) {
-                      return MeanUser.checkAdmin();
-                  }
-                }
-            }).state('themes', {
-                url: '/admin/themes',
-                templateUrl: 'admin/views/themes.html',
-                resolve: {
-                  isAdmin: function(MeanUser) {
-                      return MeanUser.checkAdmin();
-                  }
-                }
-            }).state('settings', {
-                url: '/admin/settings',
-                templateUrl: 'admin/views/settings.html',
-                resolve: {
-                  isAdmin: function(MeanUser) {
-                      return MeanUser.checkAdmin();
-                  }
-                }
-            }).state('modules', {
-                url: '/admin/modules',
-                templateUrl: 'admin/views/modules.html',
-                resolve: {
-                  isAdmin: function(MeanUser) {
-                      return MeanUser.checkAdmin();
-                  }
-                }
-            }).state('admin settings', {
-		        url: '/admin/_settings',
-		        templateUrl: 'admin/views/example.html',
-            resolve: {
-              isAdmin: function(MeanUser) {
-                  return MeanUser.checkAdmin();
-              }
-            }
-	        });
-    }
-]).config(['ngClipProvider',
-    function(ngClipProvider) {
-        ngClipProvider.setPath('../admin/assets/lib/zeroclipboard/dist/ZeroClipboard.swf');
-    }
-]);
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('users', {
+        url: '/admin/users',
+        templateUrl: 'admin/views/users.html',
+        requiredCircles: {
+          circles: ['admin']
+        }
+      }).state('themes', {
+        url: '/admin/themes',
+        templateUrl: 'admin/views/themes.html',
+        requiredCircles: {
+          circles: ['admin']
+        }
+      }).state('settings', {
+        url: '/admin/settings',
+        templateUrl: 'admin/views/settings.html',
+        requiredCircles: {
+          circles: ['admin']
+        }
+      }).state('modules', {
+        url: '/admin/modules',
+        templateUrl: 'admin/views/modules.html',
+        requiredCircles: {
+          circles: ['admin']
+        }
+      }).state('admin settings', {
+        url: '/admin/_settings',
+        templateUrl: 'admin/views/example.html',
+        requiredCircles: {
+          circles: ['admin']
+        }
+      });
+  }
+  ]).config(['ngClipProvider',
+      function(ngClipProvider) {
+          ngClipProvider.setPath('../admin/assets/lib/zeroclipboard/dist/ZeroClipboard.swf');
+      }
+  ]);
--- packages/core/articles/public/routes/articles.js
+++ packages/core/articles/public/routes/articles.js
@@ -9,37 +9,31 @@ angular.module('mean.articles').config(['$stateProvider',
       .state('all articles', {
         url: '/articles',
         templateUrl: '/articles/views/list.html',
-        resolve: {
-          loggedin: function(MeanUser) {
-            return MeanUser.checkLoggedin();
-          }
+        requiredCircles : {
+          circles: ['authenticated'],
+          denyState: 'auth.login'
         }
       })
       .state('create article', {
         url: '/articles/create',
         templateUrl: '/articles/views/create.html',
-        resolve: {
-          loggedin: function(MeanUser) {
-            return MeanUser.checkLoggedin();
-          }
+        requiredCircles : {
+          circles: ['can create content']
         }
       })
       .state('edit article', {
         url: '/articles/:articleId/edit',
         templateUrl: '/articles/views/edit.html',
-        resolve: {
-          loggedin: function(MeanUser) {
-            return MeanUser.checkLoggedin();
-          }
+        requiredCircles : {
+          circles: ['can edit content']
         }
       })
       .state('article by id', {
         url: '/articles/:articleId',
         templateUrl: '/articles/views/view.html',
-        resolve: {
-          loggedin: function(MeanUser) {
-            return MeanUser.checkLoggedin();
-          }
+        requiredCircles : {
+          circles: ['authenticated'],
+          denyState: 'auth.login'
         }
       });
   }
