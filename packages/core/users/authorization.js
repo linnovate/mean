d0ff42216d84afadb90b2751a2e65fccd67eeec1
John Webb <vapesk@gmail.com>
2015-12-24 04:29:52 -0500

    Remove requiresLogin and helper function from auth

--- packages/core/users/authorization.js
+++ packages/core/users/authorization.js
@@ -3,31 +3,6 @@ var mongoose = require('mongoose'),
   User = mongoose.model('User'),
   _ = require('lodash');
 
-
-var findUser = exports.findUser = function(id, cb) {
-  User.findOne({
-        _id: id
-    }, function(err, user) {
-        if (err || !user) return cb(null);
-        cb(user);
-    });
-};
-
-
-/**
- * Generic require login routing middleware
- */
-exports.requiresLogin = function(req, res, next) {
-  if (!req.isAuthenticated()) {
-    return res.status(401).send('User is not authorized');
-  }
-  findUser(req.user._id, function(user) {
-      if (!user) return res.status(401).send('User is not authorized');
-      req.user = user;
-      next();
-  });
-};
-
 /**
  * Generic validates if the first parameter is a mongo ObjectId
  */
