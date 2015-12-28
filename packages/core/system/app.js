e7f463160b43e6741f509295be665ac90a8ea909
John Webb <vapesk@gmail.com>
2015-12-27 19:12:18 -0500

    Created meanStarter from theme and moved users/system public content to it

--- packages/core/system/app.js
+++ packages/core/system/app.js
@@ -19,14 +19,10 @@ SystemPackage.register(function(app, auth, database, circles) {
   //We enable routing. By default the Package Object is passed to the routes
   SystemPackage.routes(app, auth, database);
 
-  SystemPackage.aggregateAsset('css', 'common.css');
   SystemPackage.angularDependencies(['ui.router', 'mean-factory-interceptor']);
   
 
   // The middleware in config/express will run before this code
-
-  // Set views path, template engine and default layout
-  app.set('views', __dirname + '/server/views');
 
   // Setting the favicon and static folder
   if(config.favicon) {
--- packages/core/system/public/routes/system.js
+++ packages/core/system/public/routes/system.js
@@ -45,25 +45,3 @@ angular.module('mean.system').provider('$meanState', ['$stateProvider', '$viewPa
 
   return new MeanStateProvider();
 }]);
-
-//Setting up route
-angular.module('mean.system').config(['$meanStateProvider', '$urlRouterProvider',
-  function($meanStateProvider, $urlRouterProvider) {
-    // For unmatched routes:
-    $urlRouterProvider.otherwise('/');
-
-    // states for my app
-    $meanStateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'system/views/index.html'
-      });
-  }
-]).config(['$locationProvider',
-  function($locationProvider) {
-    $locationProvider.html5Mode({
-      enabled:true,
-      requireBase:false
-    });
-  }
-]);
