de1122bca6876e8829bf27f2c58e324705d82a2b
John Webb <vapesk@gmail.com>
2016-01-18 12:59:04 -0500

    Moving dependencies to proper packages (reduced postinstall script)

--- tools/scripts/postinstall.js
+++ tools/scripts/postinstall.js
@@ -3,131 +3,7 @@
 var fs = require('fs'),
     npm = require('npm'),
     path = require('path'),
-    request = require('request'),
     shell  = require('shelljs');
-
-
-function getPackage(name, callback) {
-
-  var options = {
-    uri: 'https://network.mean.io/api/v0.1/packages/' + name,
-    method: 'GET'
-  };
-
-  request(options, function(error, response, body) {
-    if (!error && (response.statusCode === 200 || response.statusCode === 201)) {
-
-      if (callback) return callback(JSON.parse(body));
-
-
-    } else {
-      console.log('Package ' + name + ' not found.');
-    }
-  });
-}
-
-
-
-function install(module, options) {
-  options = options || {};
-
-  loadPackageJson('./node_modules/meanio/package.json', function(err, data) {
-
-    var destination = 'packages/contrib/';
-
-    if (err || data.version < '0.5.18') destination = 'node_modules/'; //what about the version???
-
-    var packageName = module.split('@')[0];
-
-    var tag = 'master';
-
-    tag = tag === 'latest' ? 'master' : tag;
-
-    // Don't check mean.io network if packages are installed locally in core or custom
-    fs.stat('packages/core/' + packageName, function(err, stat) {
-      if(err) {
-        fs.stat('packages/custom/' + packageName, function(err, stat) {
-          if(err) {
-            getPackage(packageName, function(data) {
-              console.log(     'Installing module: %s:', packageName);
-
-              if (data.repo.indexOf('git.mean.io') === -1) {
-                options.git = true;
-              }
-
-              var cloneUrl = options.git ? data.repo : 'https://git.mean.io/' + data.repo.split(':')[1];
-
-              shell.rm('-rf', destination + data.name);
-
-              console.log('git clone --branch ' + tag + ' ' + cloneUrl + ' ' + destination + data.name);
-
-              var silentState = shell.config.silent; // save old silent state
-              shell.config.silent = true;
-
-              shell.exec('git clone --branch ' + tag + ' --depth 1 ' + cloneUrl + ' ' + destination + data.name, function(code) {
-
-                shell.config.silent = silentState;
-
-                if (code) return console.log('Failed to clone repo: ' + cloneUrl);
-
-
-                var pkgPath = destination + data.name;
-
-                shell.rm('-rf', destination + data.name + '/.git');
-                shell.rm('-rf', destination + data.name + '/.gitignore');
-
-                // Load mean.json
-                packagesMeanJson(__dirname);
-
-                if (options.skipNpm) return;
-
-                // Load package.json
-                loadPackageJson(pkgPath + '/package.json', function(err, data) {
-                  if (err) return console.error(err);
-
-                  console.log();
-                  if (!data.mean) {
-                    console.log();
-                    console.log(     'Warning: The module installed is not a valid MEAN module');
-                  }
-
-                  shell.cd(pkgPath);
-
-                  shell.exec('npm install .', function(code) {
-
-                    if (code) return console.log(code);
-
-                    console.log(     '    Dependencies installed for package ' + data.name);
-
-                    require('bower').commands.install().on('error', function(err) {
-                      console.log('    ' + err + ' ' + data.name);
-                    });
-
-                  });
-                });
-              });
-            });
-          }
-        })
-      }
-    });
-  });
-}
-
-function packagesMeanJson(source) {
-  // Load mean.json
-  loadPackageJson(path.join(source, 'mean.json'), function(err, data) {
-    if (err || !data) return;
-
-    for (var dep in data.dependencies) {
-      shell.cd(process.cwd());
-      install(dep + '@' + data.dependencies[dep]);
-    }
-  });
-}
-
-
-
 
 function loadPackageJson(path, callback) {
   fs.readFile(path, function(err, data) {
@@ -135,7 +11,6 @@ function loadPackageJson(path, callback) {
 
     try {
       var pkg = JSON.parse(data.toString());
-      pkg.meanVersion = pkg.mean || pkg.version;
       callback(null, pkg);
     } catch (err) {
       return callback(err);
@@ -157,8 +32,6 @@ function packagesNpmInstall(source) {
 
       files.forEach(function(file) {
         var pkgPath = path.join(packages, file);
-
-        packagesMeanJson(pkgPath);
 
         loadPackageJson(path.join(pkgPath, 'package.json'), function(err, data) {
           if (err || !data.mean) return;
@@ -187,13 +60,3 @@ shell.exec('bower update', function(code) {
 packagesNpmInstall('packages/contrib');
 packagesNpmInstall('packages/custom');
 packagesNpmInstall('packages/core');
-
-// Load mean.json
-
-var source = process.cwd();
-packagesMeanJson(source);
-
-
-
-
-
