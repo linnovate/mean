5d40b6cbb7666a1762fbdebf375da186d5eba15b
pavel <pavel@linnovate.net>
2016-05-06 15:54:32 +0300

    remove aggregation from tests updated karma.conf.js to working with webpack added example of test ro meanStarter package

--- webpack.test.js
+++ webpack.test.js
@@ -1,17 +1,6 @@
 'use strict';
 
-var path = require('path');
-
-//var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
-
 module.exports = {
-    context: __dirname,
-    entry: './app.js',
-    output: {
-        path: path.join(__dirname, './bundle'),
-        publicPath: '/',
-        filename: 'app.js'
-    },
     module: {
         loaders: [
             {test: /\.css$/, loader: 'style-loader!css-loader'},
@@ -28,11 +17,5 @@ module.exports = {
     },
     resolve: {
         modulesDirectories: ['bower_components', 'node_modules']
-    },
-    plugins: [
-        /*  new ngAnnotatePlugin({
-         add: true,
-         // other ng-annotate options here
-         })*/
-    ]
+    }
 };
