9a605a5047530dcd595ff8bfb94544f5a9bed4f0
shadykeshk <shadywallas@gmail.com>
2017-04-29 01:24:58 +0200

    Fix Build Status compatibility issues in webback

--- webpack.test.js
+++ webpack.test.js
@@ -11,13 +11,13 @@ module.exports = {
     }, {
       test: /\.js$/,
       exclude: /(node_modules|bower_components|lib)/,
-      loader: 'babel?presets[]=es2015&presets[]=stage-1'
+      loader: 'babel-loader?presets[]=es2015&presets[]=stage-1'
     }, {
       test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
       loader: 'url'
     }]
   },
   resolve: {
-    modulesDirectories: ['bower_components', 'node_modules']
+    modules: ['bower_components', 'node_modules']
   }
 }
