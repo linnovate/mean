dbf25465ce587795e573921b529e4a304f8811cc
Ryan Feeney <Ryan.Feeney@pega.com>
2018-05-01 20:26:30 -0400

    BUG #1888 Fixed posts rest api

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,38 +1,81 @@
 import Post from '../models/post.model';
+import User from "../models/user.model";
 
 
-function load(params) {
-  return Post.get(params.id);
+/**
+ * Load post and append to req.
+ */
+function load(req, res, next, id) {
+  Post.get(id)
+    .then((post) => {
+      req.post = post; // eslint-disable-line no-param-reassign
+      return next();
+    })
+    .catch(e => next(e));
 }
 
+/**
+ * Get post
+ * @returns {Post}
+ */
 function get(req, res) {
   return res.json(req.post);
 }
 
-function create(params) {
+/**
+ * Create new user
+ * @property {string} req.body.title - The title of post.
+ * @property {string} req.body.content - The content of post.
+ * @returns {Post}
+ */
+function create(req, res, next) {
   const post = new Post({
-    title: params.data.title,
-    content: params.data.content
+    title: req.body.title,
+    content: req.body.content
   });
-  return post.save();
+
+  post.save()
+    .then(savedPost => res.json(savedPost))
+    .catch(e => next(e));
 }
 
-function update(params) {
-  return load(params).then(post => {
-    const tmp = post;
-    post.title = params.data.title;
-    post.content = params.data.content;
-    return post.save()
-  });
+/**
+ * Update existing post
+ * @property {string} req.body.title - The title of post.
+ * @property {string} req.body.content - The content of post.
+ * @returns {Post}
+ */
+function update(req, res, next) {
+  const post = req.post;
+  post.title = req.body.title;
+  post.content = req.body.content;
+
+  post.save()
+    .then(savedPost => res.json(savedPost))
+    .catch(e => next(e));
 }
 
-function list(params) {
-  const { limit = 50, skip = 0 } = params;
-  return Post.list({ limit, skip })
+/**
+ * Get post list.
+ * @property {number} req.query.skip - Number of posts to be skipped.
+ * @property {number} req.query.limit - Limit number of posts to be returned.
+ * @returns {Post[]}
+ */
+function list(req, res, next) {
+  const { limit = 50, skip = 0 } = req.query;
+  Post.list({ limit, skip })
+    .then(posts => res.json(posts))
+    .catch(e => next(e));
 }
 
-function remove(params) {
-  return load(params).then(post => post.remove());
+/**
+ * Delete post.
+ * @returns {Post}
+ */
+function remove(req, res, next) {
+  const post = req.post;
+  post.remove()
+    .then(deletedPost => res.json(deletedPost))
+    .catch(e => next(e));
 }
-
 export default { load, get, create, update, list, remove };
