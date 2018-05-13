bb3f7dfd73bd57c9f6a356d6aad170a09dcedf95
Orit Persik <orit@linnovate.net>
2018-05-13 12:26:19 +0300

    auth fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { AuthService } from './auth/auth.service';
 import * as schema from './schema/equipment.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  schema = schema;
-  json = {
-    name: 'something'
-  };
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  private userSubscription: Subscription;
+  private user: any;
+
+  constructor(
+    private authService: AuthService,
+  ) {}
+
+  public ngOnInit() {
+    this.userSubscription = this.authService.$userSource.subscribe((user) => {
+      this.user = user;
+    })
+  }
+
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 import { TokenStorage } from './token.storage';
 import { TooltipComponent } from '@angular/material';
@@ -10,12 +11,14 @@ export class AuthService {
 
   constructor(private http : HttpClient, private token: TokenStorage) {}
 
+  public $userSource = new Subject<any>();
+
   login(email : string, password : string) : Observable <any> {
     return Observable.create(observer => {
       this.http.post('/api/login', {
         email: email,
         password: password
-      }).subscribe(data: any => {
+      }).subscribe((data : any) => {
           observer.next({user: data.user});
           this.token.saveToken(data.token);
           observer.complete();
@@ -30,11 +33,30 @@ export class AuthService {
         email: email,
         password: password,
         repeatPassword: repeatPassword
-      }).subscribe(data => {
+      }).subscribe((data : any) => {
         observer.next({user: data.user});
+        this.setUser(data.user);
         this.token.saveToken(data.token);
         observer.complete();
       })
     });
+  }
+
+  setUser(user): void {
+    this.$userSource.next(user);
+  }
+
+  getUser(): Observable<any> {
+    return this.$userSource.asObservable();
+  }
+
+  // me(): Observable<any> {
+  //   return this.apollo.query<LoginInterface>({
+  //     query: MeQuery
+  //   });
+  // }
+
+  signOut(): void {
+    this.setUser(null);
   }
 }
