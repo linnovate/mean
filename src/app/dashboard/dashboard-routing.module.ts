ccb7226bd07ff1fa435774bb62b64e3e39ade5de
Orit Persik <orit@linnovate.net>
2018-06-12 12:12:44 +0300

    redirect to platform page

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -5,6 +5,9 @@ import { DashboardComponent } from './dashboard.component';
 import { MainSectionEntityComponent } from '../dashboard/main-section-entity/main-section-entity.component';
 
 const routes: Routes = [{
+  path: '',
+  redirectTo: 'platform',
+},{
   path: ':type',
   component: DashboardComponent,
   children: [{
