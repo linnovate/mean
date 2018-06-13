71859019e0c5367eaf1a39c89c589a1cc78c2217
Orit Persik <orit@linnovate.net>
2018-06-13 13:00:16 +0300

    system routes added

---
+++ src/app/dashboard/system-sidebar/system-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-system-sidebar',
+  templateUrl: './system-sidebar.component.html',
+  styleUrls: ['./system-sidebar.component.scss']
+})
+export class SystemSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
