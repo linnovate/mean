d0ca09202104932e77f00f0c8bc797c8f20c1781
Avraham Ostreicher <avraham@linnovate.net>
2018-06-13 17:39:18 +0300

    Sidebar-actions - icons bar

--- src/app/dashboard/sidebar-actions/sidebar-actions.component.ts
+++ src/app/dashboard/sidebar-actions/sidebar-actions.component.ts
@@ -7,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarActionsComponent implements OnInit {
 
+  icons: string[] = [
+    'airplane-front-view',
+    'air-station',
+    'balloon',
+    'boat',
+    'cargo-ship',
+    'car',
+    'catamaran',
+    'convertible',
+    'drone',
+    'fighter-plane',
+    'fire-truck',
+    'horseback-riding',
+    'motorcycle',
+    'railcar',
+    'railroad-train',
+    'rocket-boot',
+    'sailing-boat',
+    'segway',
+    'shuttle',
+    'space-shuttle',
+    'steam-engine',
+    'suv',
+    'tour-bus',
+    'tow-truck',
+    'transportation',
+    'trolleybus',
+    'water-transportation',
+    '',
+    '',
+    '',
+    '',
+    '',
+    ''
+  ]; // empty icons are to align last flexbox row to the left
+
+  iconsToDisplay: string[] = this.icons;
+
+  filterIcons(input) {
+    this.iconsToDisplay = this.icons.filter(icon => {
+      // don't filter out empty icons (`!icon`)
+      return !icon || icon.includes(input.value);
+    })
+  }
+
   constructor() { }
 
   ngOnInit() {
