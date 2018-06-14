bcb003caab80ec5796eb9589055e64fe6cfa6b29
Orit Persik <orit@linnovate.net>
2018-06-14 11:33:16 +0300

    fix dragula model

--- src/app/dashboard/main-section-system/main-section-system.component.ts
+++ src/app/dashboard/main-section-system/main-section-system.component.ts
@@ -9,14 +9,26 @@ import { DragulaService } from 'ng2-dragula';
 })
 export class MainSectionSystemComponent implements OnInit {
 
+
   name: string = '';
   platform: any;
   equipment: Array<any>;
-  system: any = {}
+  platforms: Array<any> = [];
+  system = {
+    name: '',
+    description: '',
+    platform: [],
+    equipment: [],
+  };
 
   constructor(private dragulaService: DragulaService) {
-    dragulaService.drop.subscribe((value) => {
-      this.system.platform = value.slice(1)[0].lastChild.innerText;
+    dragulaService.dropModel.subscribe((value) => {
+      console.log(this.system);
+      // this.system.platform = value.slice(1)[0].lastChild.innerText;
+    });
+    dragulaService.removeModel.subscribe((value) => {
+      console.log(this.system);
+      // this.system.platform = value.slice(1)[0].lastChild.innerText;
     });
     dragulaService.setOptions('platform', {
       invalid: this.invalidHandler
@@ -24,9 +36,9 @@ export class MainSectionSystemComponent implements OnInit {
    }
 
    invalidHandler(el, handle) {
-    let draggedFromMain = el;
-    while ((draggedFromMain = draggedFromMain.parentElement) && !draggedFromMain.classList.contains('main-section'));
-    if ((this as any).containers[0].childElementCount === 1 && !draggedFromMain) return true;
+    // let draggedFromMain = el;
+    // while ((draggedFromMain = draggedFromMain.parentElement) && !draggedFromMain.classList.contains('main-section'));
+    // if ((this as any).containers[0].childElementCount === 1 && !draggedFromMain) return true;
     return false;
   }
 
