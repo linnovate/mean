4b0e986982f33f9e681442ff1d8a58cdc332165b
Orit Persik <orit@linnovate.net>
2018-06-17 10:49:34 +0300

    new system

--- src/app/dashboard/main-section-system/main-section-system.component.ts
+++ src/app/dashboard/main-section-system/main-section-system.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DragulaService } from 'ng2-dragula';
+import { SystemService } from '../services/system.service';
 
 
 @Component({
@@ -11,6 +12,7 @@ export class MainSectionSystemComponent implements OnInit {
 
 
   name: string = '';
+  description: string = '';
   platform: any;
   equipment: Array<any>;
   platforms: Array<any> = [];
@@ -19,19 +21,26 @@ export class MainSectionSystemComponent implements OnInit {
     description: '',
     platform: [],
     equipment: [],
+    status: 'draft'
   };
+  statuses: string[] = ['draft', 'waiting', 'approved', 'rejected'];
+  status: string;
+  displayEquipmentPlaceHolder: string = 'flex';
 
-  constructor(private dragulaService: DragulaService) {
+  constructor(private dragulaService: DragulaService, private systemService: SystemService) {
     dragulaService.dropModel.subscribe((value) => {
+      if (value[0] === 'equipment') this.displayEquipmentPlaceHolder = 'none';
       console.log(this.system);
-      // this.system.platform = value.slice(1)[0].lastChild.innerText;
     });
     dragulaService.removeModel.subscribe((value) => {
       console.log(this.system);
-      // this.system.platform = value.slice(1)[0].lastChild.innerText;
+    });
+    dragulaService.drag.subscribe((value) => {
+      if (value[0] === 'equipment') this.displayEquipmentPlaceHolder = 'flex';
+      console.log(value,  'orit');
     });
     dragulaService.setOptions('platform', {
-      invalid: this.invalidHandler
+      //invalid: this.invalidHandler
     });
    }
 
@@ -42,8 +51,25 @@ export class MainSectionSystemComponent implements OnInit {
     return false;
   }
 
-  ngOnInit() {
+  cancel() {}
+
+  save() {
+    this.system.name = this.name;
+    this.system.description = this.description;
+    if (this.valid())
+      this.systemService.save(this.system).subscribe(data => {
+        console.log('saved system', data);
+      });
   }
+
+  valid() {
+    if (this.system.platform.length === 1 && this.system.equipment.length > 0) return true;
+    return false;
+  }
+
+  delete() {}
+
+  ngOnInit() {}
 
   ngOnDestroy() {
     this.dragulaService.destroy('platform');
