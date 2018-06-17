950f8958137fcea885fc3b1db4052ea83e5a3366
Orit Persik <orit@linnovate.net>
2018-06-17 11:54:53 +0300

    remove item from system lists

--- src/app/dashboard/sidebar-system/sidebar-system.component.ts
+++ src/app/dashboard/sidebar-system/sidebar-system.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SchemaService } from '../services/schema.service';
+import { SystemService } from '../services/system.service';
 import { DragulaService } from 'ng2-dragula';
 
 @Component({
@@ -14,23 +15,43 @@ export class SidebarSystemComponent implements OnInit {
 
   constructor(
     private schemaService: SchemaService,
-    private dragulaService: DragulaService) {
-      
-      ['platform', 'equipment'].forEach(type => {
-        this.schemaService.tree(type, 'name').subscribe(data => {
-          data.map(category => {
-            category.children.map(child => {
-              child.category = category.name;
-              return child;
-            });
-            return category;
-          });
-          this.data[type] = data
-        });
-      });
+    private dragulaService: DragulaService,
+    private systemService: SystemService) {
+      this.init();
+      this.dragulaEvents();
+      this.subscribeEvents();
+  }
 
-    dragulaService.dragend.subscribe((value) => {
+  dragulaEvents() {
+    this.dragulaService.dragend.subscribe((value) => {
       if (value[0] === 'platform') this.showPlatform = false;
+    });
+  }
+
+  init() {
+    ['platform', 'equipment'].forEach(type => {
+      this.schemaService.tree(type, 'name').subscribe(data => {
+        data.map(category => {
+          category.children.map(child => {
+            child.category = category.name;
+            return child;
+          });
+          return category;
+        });
+        this.data[type] = data
+      });
+    });
+  }
+
+  subscribeEvents() {
+    this.systemService.events.subscribe(event => {
+      switch(event.name) {
+        case 'item.deleted': {
+          let categoryIndex = this.data[event.data.type].findIndex(e => e.category === event.data.item.category);
+          this.data[event.data.type][categoryIndex].children.push(event.data.item);
+          break;
+        }
+      }
     });
   }
 
