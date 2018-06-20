3e6651e45b9b70f5ab0545e4ec297b6aa77906a0
Orit Persik <orit@linnovate.net>
2018-06-20 10:31:27 +0300

    clean mongoose warning

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -6,7 +6,7 @@ const config = require('./config');
 
 // connect to mongo db
 const mongoUri = config.mongo.host;
-mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } });
+mongoose.connect(mongoUri, { keepAlive: 1 });
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`);
 });
