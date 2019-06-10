e6f8634d606f17f6be9105e6a9dcb19831e35ca0
andrey1103 <46831252+andrey1103@users.noreply.github.com>
2019-06-10 03:04:55 +0300

    Update mongoose.js

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -6,7 +6,8 @@ const config = require('./config');
 
 // connect to mongo db
 const mongoUri = config.mongo.host;
-mongoose.connect(mongoUri, { keepAlive: 1 });
+mongoose.set('useCreateIndex', true);
+mongoose.connect(mongoUri, { keepAlive:1, useNewUrlParser: true});
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`);
 });
