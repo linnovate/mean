a3612ea1137270bb4636c76ceafaf8131ec0792e
Artur Androsovych <arthurandrosovich@gmail.com>
2020-12-07 19:16:44 +0000

    feat: upgrade to Angular 11 (#1998)

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 
@@ -6,7 +6,7 @@ describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HeaderComponent ]
     })
