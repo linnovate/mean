19307c2ceb7a0dc7ba34d9fa6c71912bbb39df9d
James H <53068780+Jimson91@users.noreply.github.com>
2020-12-08 05:19:39 +1000

    Fix Mongoose Deprecation Warnings (#1972)

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -6,7 +6,12 @@ const config = require('./config');
 
 // connect to mongo db
 const mongoUri = config.mongo.host;
-mongoose.connect(mongoUri, { keepAlive: 1 });
+mongoose.connect(mongoUri, { 
+  keepAlive: 1,
+  useCreateIndex: true,
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`);
 });
